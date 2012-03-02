e089e26c82c12e11d06b439a496a4d468187212e
David O'Trakoun <me@davidosomething.com>
2012-03-02 15:04:53 -0500

    fix vim updating

--- update.sh
+++ update.sh
@@ -6,13 +6,17 @@ echo "| Updating zsh                                                            
 echo "+------------------------------------------------------------------------------+"
 cd ~/.dotfiles
 git pull
-echo
-echo "+------------------------------------------------------------------------------+"
-echo "| Updating vim                                                                 |"
-echo "+------------------------------------------------------------------------------+"
-cd ~/.vim
-git pull
-git submodule update
+
+if [[ -x ~/.vim/update.sh ]]; then
+  cd ~/.vim
+  echo
+  echo "+------------------------------------------------------------------------------+"
+  echo "| Updating .vim folder and .vimrc                                              |"
+  echo "+------------------------------------------------------------------------------+"
+  git pull          # update vim folder and vimrc itself
+  ~/.vim/update.sh  # update submodules
+fi
+
 echo
 echo "DONE!"
 echo
