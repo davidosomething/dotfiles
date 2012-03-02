b9b8939bcc35c15f5776b3dae71e89dc618f38fe
David O'Trakoun <me@davidosomething.com>
2012-03-02 13:44:30 -0500

    swap grep and output redirection

--- install.sh
+++ install.sh
@@ -14,7 +14,7 @@ p="git";        if ! which $p >/dev/null 2>&1;then echo "[MISSING] $p";return 1;
 echo -n "Set up ssh keys [y/N]? ";                    read do_ssh;
 echo -n "Set up git and github [y/N]? ";              read do_git;
 [ "$do_git:l" != "y" ] && {
-  if ! cat ~/.gitconfig|grep token >/dev/null 2>&1; then
+  if ! cat ~/.gitconfig >/dev/null 2>&1 | grep token; then
     echo '[MISSING] github token, set up git!'
     return 1;
   fi
