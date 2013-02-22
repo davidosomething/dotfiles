e608b866d5e83ac30ec9d98dbc1588a0a005857d
David O'Trakoun <me@davidosomething.com>
2013-02-22 16:38:56 -0500

    update all kinds of things for compatibility and proper use of ZDOTDIR and ZSH_DOTFILES folders (they're different!)

--- bootstrap/zsh.sh
+++ bootstrap/zsh.sh
@@ -1,1 +1,16 @@
-chsh -s /bin/zsh
+#!/bin/bash
+set -e
+
+# on OSX:
+# brew install zsh zsh-completions
+
+# on ALL:
+
+# echo /usr/local/bin/zsh | sudo tee -a /etc/shells
+
+# ASSUME PATH INCLUDES /usr/local/bin
+# chsh -s `which zsh`
+
+# OTHERWISE
+# OSX: chsh -s /usr/local/bin/zsh
+# *NIX: chsh -s /bin/zsh
