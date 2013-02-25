4ca513165c258bb9b4397f860467b9eb0c14c030
David O'Trakoun <me@davidosomething.com>
2013-02-25 00:18:52 -0500

    add github function

--- bash/functions-osx.sh
+++ bash/functions-osx.sh
@@ -9,9 +9,27 @@ function mvim() {
 }
 
 function github() {
-  local remotes
-  if git remote -v; then
-    #remotes="`git remote -v | grep "\(fetch\)$""
-    echo "error: No remotes found or not a repository."
+  local github_url
+
+  if ! git remote -v >/dev/null; then
+    return 1
   fi
+
+  # get remotes for fetch
+  github_url="`git remote -v | grep github\.com | grep \(fetch\)$`"
+
+  if [ -z "$github_url" ]; then
+    echo "A GitHub remote was not found for this repository."
+    return 1
+  fi
+
+  # look for origin in remotes, use that if found, otherwise use first result
+  if [ "echo $github_url | grep '^origin' >/dev/null 2>&1" ]; then
+    github_url="`echo $github_url | grep '^origin'`"
+  else
+    github_url="`echo $github_url | head -n1`"
+  fi
+
+  github_url="`echo $github_url | awk '{ print $2 }' | sed 's/git@github\.com:/http:\/\/github\.com\//g'`"
+  open $github_url
 }
