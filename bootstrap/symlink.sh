9bb7f50d9450a40f193fc20ef7c72cb666795f0a
David O'Trakoun <me@davidosomething.com>
2013-02-27 11:09:09 -0500

    update symlinking rules

--- bootstrap/symlink.sh
+++ bootstrap/symlink.sh
@@ -4,12 +4,16 @@ set -e
 function symlink {
   ln -fns $DOTFILES_FOLDER/$1 $HOME/$2 && status "$2 symlinked to $DOTFILES_FOLDER/$1"
 }
+
 status "Symlinking dotfiles"
-symlink ack/ackrc           .ackrc
-symlink bash/bashrc         .bashrc
-symlink bash/bash_profile   .bash_profile
-symlink pow/.powconfig      .powconfig
-symlink tmux/tmux.conf      .tmux.conf
-symlink screen/screenrc     .screenrc
-symlink zsh/zshenv          .zshenv
-symlink zsh/zshrc           .zshrc
+symlink ack/ackrc             .ackrc
+
+symlink tmux/tmux.conf        .tmux.conf
+
+symlink screen/screenrc       .screenrc
+
+symlink bash/bashrc.sh        .bashrc
+symlink bash/bash_profile.sh  .bash_profile
+
+symlink zsh                   .zsh
+symlink zsh/zshenv.zsh        .zshenv
