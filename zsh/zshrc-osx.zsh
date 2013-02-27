0cbf3fbca090945d6af276ad824a9fc9399edd5a
David O'Trakoun <me@davidosomething.com>
2013-02-27 11:04:34 -0500

    load bash completion into zsh

--- zsh/zshrc-osx.zsh
+++ zsh/zshrc-osx.zsh
@@ -1,7 +1,15 @@
-# .dotfiles/zsh/zshrc-osx
+# .dotfiles/zsh/zshrc-osx.zsh
 # sourced by .zshrc
 
 # rbenv completions (can also add to bash, but it's not there)
 if which rbenv > /dev/null; then
   eval "$(rbenv init -)"
+fi
+
+################################################################################
+# Git completion
+autoload bashcompinit && bashcompinit
+# if brew command exists, source bash completion from homebrew folder
+if [[ $+commands[brew] == 1 ]]; then
+  source `brew --prefix`/etc/bash_completion.d
 fi
