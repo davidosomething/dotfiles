bf89234ab4882b2b045c81fba765b4a5ac57205b
David O'Trakoun <me@davidosomething.com>
2013-03-03 15:11:26 -0500

    only load certain completions if they exist

--- bash/completions.sh
+++ bash/completions.sh
@@ -1,16 +1,11 @@
 ################################################################################
 # bash completion
-if [ -r /etc/bash_completion ]; then
-  source /etc/bash_completion
-fi
-
-if [ -r /usr/share/bash-completion/bash_completion ]; then
-  source /usr/share/bash-completion/bash_completion
-fi
+[ -f /etc/bash_completion ] && source /etc/bash_completion
+[ -f /usr/share/bash-completion/bash_completion ] && source /usr/share/bash-completion/bash_completion
 
 # if brew command exists, source bash completion from homebrew folder
-source /usr/local/etc/bash_completion.d/git-completion.bash
-source /usr/local/etc/bash_completion.d/hub.bash_completion.sh
-source /usr/local/etc/bash_completion.d/tmux
-source /usr/local/etc/bash_completion.d/vagrant
-source /usr/local/etc/bash_completion.d/wp
+[ -f /usr/local/etc/bash_completion.d/git-completion.bash ] && source /usr/local/etc/bash_completion.d/git-completion.bash
+[ -f /usr/local/etc/bash_completion.d/hub.bash_completion.sh ] && source /usr/local/etc/bash_completion.d/hub.bash_completion.sh
+[ -f /usr/local/etc/bash_completion.d/tmux ] && source /usr/local/etc/bash_completion.d/tmux
+[ -f /usr/local/etc/bash_completion.d/vagrant ] && source /usr/local/etc/bash_completion.d/vagrant
+[ -f /usr/local/etc/bash_completion.d/wp ] && source /usr/local/etc/bash_completion.d/wp
