33a57ca07be878e987889b8180b0c16f7dba5f72
David O'Trakoun <me@davidosomething.com>
2013-03-13 11:13:17 -0400

    symlinking status function colors

--- bootstrap/helpers.sh
+++ bootstrap/helpers.sh
@@ -10,7 +10,7 @@ function dkostatus_()    { echo -e "\033[0;32m    $*\033[0;m"; }
 function dkoerr()        { echo -e "\033[0;31m==> \033[0;33mERROR: \033[0;31m$*\033[0;m"; }
 function dkoerr_()       { echo -e "\033[0;31m    $*\033[0;m"; }
 function dkoinstalling() { dkostatus "Installing \033[0;33m$1\033[0;32m..."; }
-function dkosymlinking() { dkostatus "Symlinking \033[0;33m$1\033[0;32m -> \033[0;33m$2\033[0;32m "; }
+function dkosymlinking() { dkostatus "Symlinking \033[0;35m$1\033[0;32m -> \033[0;35m$2\033[0;32m "; }
 function dkodie()        { dkoerr "$*"; exit 256; }
 
 function dkorequire()    {
