a85b1587581cae8628b54a4137a9e4494e79529c
David O'Trakoun <me@davidosomething.com>
2013-03-13 11:11:20 -0400

    make sure helper functions use prefixes

--- bootstrap/helpers.sh
+++ bootstrap/helpers.sh
@@ -9,14 +9,14 @@ function dkostatus()     { echo -e "\033[0;34m==>\033[0;32m $*\033[0;m"; }
 function dkostatus_()    { echo -e "\033[0;32m    $*\033[0;m"; }
 function dkoerr()        { echo -e "\033[0;31m==> \033[0;33mERROR: \033[0;31m$*\033[0;m"; }
 function dkoerr_()       { echo -e "\033[0;31m    $*\033[0;m"; }
-function dkoinstalling() { status "Installing \033[0;33m$1\033[0;32m..."; }
-function dkosymlinking() { status "Symlinking \033[0;33m$1\033[0;32m -> \033[0;33m$2\033[0;32m "; }
-function dkodie()        { err "$*"; exit 256; }
+function dkoinstalling() { dkostatus "Installing \033[0;33m$1\033[0;32m..."; }
+function dkosymlinking() { dkostatus "Symlinking \033[0;33m$1\033[0;32m -> \033[0;33m$2\033[0;32m "; }
+function dkodie()        { dkoerr "$*"; exit 256; }
 
 function dkorequire()    {
   if [ command -v $1 >/dev/null 2>&1 ]; then
-    status "FOUND: $1"
+    dkostatus "FOUND: $1"
   else
-    die "missing $1, please install before proceeding.";
+    dkodie "missing $1, please install before proceeding.";
   fi
 }
