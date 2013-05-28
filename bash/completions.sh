8bd2e17c08226862ef89b36b6451928ebc0a4b45
David O'Trakoun <me@davidosomething.com>
2013-05-28 10:29:31 -0400

    don't source macports bash completion in bash_profile, only source brew paths if brew cmd exists

--- bash/completions.sh
+++ bash/completions.sh
@@ -6,8 +6,11 @@ function source_if_exists() {
 
 source_if_exists /etc/bash_completion
 source_if_exists /usr/share/bash-completion/bash_completion
-source_if_exists $(brew --prefix)/etc/bash_completion.d/git-completion.bash
-source_if_exists $(brew --prefix)/etc/bash_completion.d/hub.bash_completion.sh
-source_if_exists $(brew --prefix)/etc/bash_completion.d/tmux
-source_if_exists $(brew --prefix)/etc/bash_completion.d/vagrant
-source_if_exists $(brew --prefix)/etc/bash_completion.d/wp
+
+command -v brew >/dev/null 2>&1 && command -v awk >/dev/null 2>&1 && {
+  source_if_exists $(brew --prefix)/etc/bash_completion.d/git-completion.bash
+  source_if_exists $(brew --prefix)/etc/bash_completion.d/hub.bash_completion.sh
+  source_if_exists $(brew --prefix)/etc/bash_completion.d/tmux
+  source_if_exists $(brew --prefix)/etc/bash_completion.d/vagrant
+  source_if_exists $(brew --prefix)/etc/bash_completion.d/wp
+}
