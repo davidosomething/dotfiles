da110555888f2c72c2d24605ef4b5ef0a841692c
David O'Trakoun <me@davidosomething.com>
2013-06-14 19:37:19 -0400

    add sysup command, update vundle and zsh-completion submodules

--- bash/aliases-osx.sh
+++ bash/aliases-osx.sh
@@ -5,7 +5,7 @@
 # osx aliases since -G is an osx (and FreeBSD) flag
 alias ls="ls -AFG"
 alias ll="ls -l"
-alias flushdns="dscacheutil -flushcache"
+alias flushdns="dscacheutil -flushcache && sudo killall -HUP mDNSResponder"
 
 # don't use mvim on ssh connections
 if [ ! -z "$SSH_CONNECTION" ]; then
@@ -22,3 +22,5 @@ alias a2r="sudo apachectl -k restart"
 # http://xkcd.com/530/
 alias stfu="osascript -e 'set volume output muted true'"
 alias pumpitup="osascript -e 'set volume 7'"
+
+alias sysup="sudo softwareupdate -i -a"
