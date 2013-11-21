6d1dfb9805290eebd9e734bd0ca74492466ce94d
David O'Trakoun <me@davidosomething.com>
2013-11-21 03:52:18 -0500

    remove dotfiles alias, add composer installation and alias for linux

--- bash/aliases-linux.sh
+++ bash/aliases-linux.sh
@@ -1,9 +1,11 @@
 ##
 # .dotfiles/bash/aliases-linux.sh
 
+# ls
 alias ls="ls --color=auto"
 alias ll="ls -Al"
 
+# vim
 # don't use mvim on ssh connections and tty
 if [ "$SSH_CONNECTION" != "" ] || [ "$DISPLAY" == "" ]; then
   alias e="vim"
@@ -14,6 +16,12 @@ else
   alias mvim="gvim"
 fi
 
+# gui
 alias open='xdg-open'
 
+################################################################################
+# dev
+alias composer="php $HOME/.composer/bin/composer.phar"
+# web
 alias a2r="sudo service apache2 restart"
+
---
+++ bootstrap/linux/composer.sh
@@ -0,0 +1,2 @@
+mkdir -p $HOME/.composer/bin
+curl -sS https://getcomposer.org/installer | php -- --install-dir=$HOME/.composer/bin
