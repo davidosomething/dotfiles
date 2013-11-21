7038fc21f99218118ac4e5abc0f161533cf2c884
David O'Trakoun <me@davidosomething.com>
2013-11-21 03:56:45 -0500

    composer installation options

--- bootstrap/linux/composer.sh
+++ bootstrap/linux/composer.sh
@@ -1,2 +1,17 @@
-mkdir -p $HOME/.composer/bin
-curl -sS https://getcomposer.org/installer | php -- --install-dir=$HOME/.composer/bin
+# install locally
+if [[ "$1" = "--local" ]]; then
+  mkdir -p $HOME/.composer/bin
+  curl -sS https://getcomposer.org/installer | php -- --install-dir=$HOME/.composer/bin
+  exit 0
+
+elif [[ $EUID -ne 0 ]]; then
+  echo "Pass --local to install locally, or run as root to install globally." 1>&2
+  exit 1
+
+# install globally
+else
+  curl -sS https://getcomposer.org/installer | php
+  mv composer.phar /usr/local/bin/composer
+  exit 0
+
+fi
