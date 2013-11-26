e28e86734255e36a67a290351e05c8d108348183
David O'Trakoun <me@davidosomething.com>
2013-11-26 02:29:32 -0500

    move update script to bootstrap, symlink from bin, add helpers to update, check for git dirty before update dotfiles

---
+++ bootstrap/update.sh
@@ -0,0 +1,51 @@
+#!/usr/bin/env bash
+#
+# Update dotfiles and provide instructions for updating the system
+
+set -eu
+
+################################################################################
+# initialize script and dependencies
+# get this bootstrap folder
+pushd "$(dirname $0)"/.. >> /dev/null
+dotfiles_path="$(pwd)"
+bootstrap_path="$dotfiles_path/bootstrap"
+source $bootstrap_path/helpers.sh
+popd >> /dev/null
+
+##
+# Return number of untracked files in git
+function evil_git_num_untracked_files {
+  expr `git status --porcelain 2>/dev/null| grep "^??" | wc -l` 
+}
+
+# Make sure there are no untracked changes before updating dotfiles
+if [[ ! -n $(evil_git_num_untracked_files) ]]; then
+  dkodie "You have unsaved changes in your ~/.dotfiles folder."
+fi
+
+# Update dotfiles
+dkostatus "Jumping to dotfiles directory" && pushd $HOME/.dotfiles >> /dev/null
+dkostatus "Getting latest updates" && git stash && git pull --rebase --recurse-submodules && git stash apply
+dkostatus "Back to original directory" popd >> /dev/null
+
+if [[ $OSTYPE = "linux-gnu" ]]; then
+
+  :
+
+else
+  dkostatus "You should run these commands as needed:"
+  echo "diskutil repairPermissions /  # fix file system permissions"
+  echo "sysup                         # alias for osx software update"
+  echo "brew doctor"
+  echo "brew update"
+  echo "brew upgrade"
+  echo "brew cleanup"
+  echo "gem update --system"
+  echo "gem update"
+  echo "gem clean"
+  echo "npm update -g"
+  echo "heroku update"
+  echo "wpcliup                       # update wp-cli"
+  echo "vimup"                        # alias that updates vim plugins"
+fi
