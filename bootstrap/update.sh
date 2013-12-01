6ea8e77f761b932be216912b506ec21b140689ce
David O'Trakoun <me@davidosomething.com>
2013-12-01 04:59:08 -0500

    add flags to update script, sort vimrc by neobundle plugin

--- bootstrap/update.sh
+++ bootstrap/update.sh
@@ -1,4 +1,4 @@
-#!/usr/bin/env bash
+#!/bin/bash
 #
 # Update dotfiles and provide instructions for updating the system
 
@@ -19,15 +19,17 @@ function evil_git_num_untracked_files {
   expr `git status --porcelain 2>/dev/null| grep "^??" | wc -l` 
 }
 
-# Make sure there are no untracked changes before updating dotfiles
-if [[ ! -n $(evil_git_num_untracked_files) ]]; then
-  dkodie "You have unsaved changes in your ~/.dotfiles folder."
-fi
+if [ $# -eq 0 ]; then
+  # Make sure there are no untracked changes before updating dotfiles
+  if [[ ! -n $(evil_git_num_untracked_files) ]]; then
+    dkodie "You have unsaved changes in your ~/.dotfiles folder."
+  fi
 
-# Update dotfiles
-dkostatus "Jumping to dotfiles directory" && pushd $HOME/.dotfiles >> /dev/null
-dkostatus "Getting latest updates" && git stash && git pull --rebase --recurse-submodules && git stash apply
-dkostatus "Back to original directory" popd >> /dev/null
+  # Update dotfiles
+  dkostatus "Jumping to dotfiles directory" && pushd $HOME/.dotfiles >> /dev/null
+  dkostatus "Getting latest updates" && git stash && git pull --rebase --recurse-submodules && git stash apply
+  dkostatus "Back to original directory" popd >> /dev/null
+fi
 
 dkostatus "You should run these commands as needed:"
 if [[ $OSTYPE = "linux-gnu" ]]; then
@@ -39,16 +41,28 @@ else
   echo "[osx only]"
   echo "diskutil repairPermissions /  # fix file system permissions"
   echo "sysup                         # alias for osx software update"
-  echo "brew doctor"
-  echo "brew update"
-  echo "brew upgrade"
-  echo "brew cleanup"
+
+  if [[ "$1" = "brew" ]]; then
+    brew doctor && brew update && brew upgrade && brew cleanup
+
+  else
+    echo "brew doctor"
+    echo "brew update"
+    echo "brew upgrade"
+    echo "brew cleanup"
+  fi
 fi
 
 echo "[common]"
-echo "gem update --system"
-echo "gem update"
-echo "gem clean"
+
+if [[ "$1" = "gem" ]]; then
+  gem update --system && gem update && gem clean
+else
+  echo "gem update --system"
+  echo "gem update"
+  echo "gem clean"
+fi
+
 echo "npm update -g"
 echo "heroku update"
 echo "vimup"                        # alias that updates vim plugins"
