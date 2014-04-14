153bfaba8c566bc83c01015bc7bc75cd85bb32bd
David O'Trakoun <me@davidosomething.com>
2014-04-14 16:33:51 -0400

    perm change for completions, add path function (zsh only though)

--- zsh/functions.zsh
+++ zsh/functions.zsh
@@ -1,13 +1,3 @@
 # specific to zsh!
 # sourced by .zshrc
 
-# colored path from https://github.com/myfreeweb/zshuery/blob/master/zshuery.sh
-path() {
-  echo $PATH | tr ":" "\n" | \
-    awk "{ sub(\"/usr\",   \"$fg_no_bold[green]/usr$reset_color\"); \
-           sub(\"/bin\",   \"$fg_no_bold[blue]/bin$reset_color\"); \
-           sub(\"/opt\",   \"$fg_no_bold[cyan]/opt$reset_color\"); \
-           sub(\"/sbin\",  \"$fg_no_bold[magenta]/sbin$reset_color\"); \
-           sub(\"/local\", \"$fg_no_bold[yellow]/local$reset_color\"); \
-           print }"
-}
