09f2e6ef83a6e74afd483260883d17e76bfd0572
David O'Trakoun <me@davidosomething.com>
2014-04-14 23:45:09 -0400

    open up braces in functions-linux.sh

--- bash/functions-linux.sh
+++ bash/functions-linux.sh
@@ -1,5 +1,14 @@
-a2() { sudo service apache2 $@ }
-a2r() { sudo service apache2 restart }
-composer() { php $HOME/.composer/bin/composer.phar $@ }
+a2() {
+  sudo service apache2 $@
+}
+a2r() {
+  sudo service apache2 restart
+}
+composer() {
+  php $HOME/.composer/bin/composer.phar $@
+}
+
 # gui
-open() { xdg-open $@ }
+open() {
+  xdg-open $@
+}
