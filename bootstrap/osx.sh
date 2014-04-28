06c1f71a430d984aaf9da65e631c38874f0cd124
David O'Trakoun <me@davidosomething.com>
2014-04-28 16:32:33 -0400

    fix symlink of php plist

--- bootstrap/osx.sh
+++ bootstrap/osx.sh
@@ -3,7 +3,7 @@ rbenv install 2.1.1
 rbenv global 2.1.1
 
 # After homebrew PHP
-ln -sfv /usr/local/opt/php55/*.plist
+ln -sfv /usr/local/opt/php55/*.plist ~/Library/LaunchAgents
 launchctl load ~/Library/LaunchAgents/homebrew.mxcl.php55.plist
 
 ln -sfv /usr/local/opt/memcached/*.plist ~/Library/LaunchAgents
