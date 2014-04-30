c7f27c6c548c5a0b96a7a020d8136efd6e7394a9
David O'Trakoun <me@davidosomething.com>
2014-04-30 03:29:46 -0400

    remove zsh.sh from bootstrap

--- bootstrap/osx.sh
+++ bootstrap/osx.sh
@@ -7,3 +7,6 @@ ln -sfv /usr/local/opt/php55/*.plist ~/Library/LaunchAgents
 launchctl load ~/Library/LaunchAgents/homebrew.mxcl.php55.plist
 
 ln -sfv /usr/local/opt/memcached/*.plist ~/Library/LaunchAgents
+
+# after brew install zsh
+echo /usr/local/bin/zsh | sudo tee -a /etc/shells
