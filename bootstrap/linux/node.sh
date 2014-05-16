4a8286be5c59fc21ca5b46d10cefab80c39d82bf
David O'Trakoun <me@davidosomething.com>
2014-05-16 19:11:36 -0400

    upd linux bootstrapping

--- bootstrap/linux/node.sh
+++ bootstrap/linux/node.sh
@@ -1,3 +1,2 @@
-sudo aptitude install nodejs
-sudo aptitude install npm
+sudo aptitude install nodejs nodejs-legacy npm
 sudo ln -s /usr/bin/nodejs /usr/bin/node
---
+++ bootstrap/linux/shutter.sh
@@ -0,0 +1,3 @@
+sudo add-apt-repository ppa:shutter/ppa
+sudo aptitude update
+sudo aptitude install shutter
