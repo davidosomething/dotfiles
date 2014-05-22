e88ced53a1208472c2699af68fc941dc0032166c
itloaner <itloaner@loaner113.arn.com>
2014-05-22 15:16:52 -0400

    add co alias to git

--- bootstrap/git.sh
+++ bootstrap/git.sh
@@ -2,4 +2,5 @@ git config --global push.default simple
 git config --global alias.b   branch
 git config --global alias.ci  commit
 git config --global alias.cm  'commit -m'
+git config --global alias.co  checkout
 git config --global alias.st  status
