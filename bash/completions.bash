57f34f6a77c56c8effe7d2f4294a0950f6e5c06c
David O'Trakoun <me@davidosomething.com>
2014-06-11 19:55:49 -0400

    quote grunt in has_program, linux bootstrap nvm default

--- bash/completions.bash
+++ bash/completions.bash
@@ -9,7 +9,7 @@ source_if_exists /usr/share/bash-completion/bash_completion
 source_if_exists $HOME/.wp-cli/vendor/wp-cli/wp-cli/utils/wp-completion.bash
 
 # grunt
-has_program grunt && eval "$(grunt --completion=bash)"
+has_program "grunt" && eval "$(grunt --completion=bash)"
 
 # homebrew's bash-completion package sources the rest of bash_completion.d
 [ -n $HAS_BREW ] && source_if_exists "$BREW_PREFIX/etc/bash_completion"
--- bootstrap/linux/node.sh
+++ bootstrap/linux/node.sh
@@ -1,2 +1,4 @@
-sudo aptitude install nodejs nodejs-legacy npm
-sudo ln -s /usr/bin/nodejs /usr/bin/node
+curl https://raw.githubusercontent.com/creationix/nvm/v0.7.0/install.sh | sh
+nvm install 0.10
+nvm alias default 0.10
+nvm use 0.10
