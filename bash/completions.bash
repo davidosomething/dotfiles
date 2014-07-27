789b60bde94027d1e5ffd812f35b62bdc8d8de3c
David O'Trakoun <me@davidosomething.com>
2014-07-27 14:49:59 -0400

    travis bash completion

--- bash/completions.bash
+++ bash/completions.bash
@@ -11,5 +11,8 @@ source_if_exists $HOME/.wp-cli/vendor/wp-cli/wp-cli/utils/wp-completion.bash
 # grunt
 has_program "grunt" && eval "$(grunt --completion=bash)"
 
+# travis
+source_if_exists ~/.travis/travis.sh
+
 # homebrew's bash-completion package sources the rest of bash_completion.d
 [ -n $HAS_BREW ] && source_if_exists "$BREW_PREFIX/etc/bash_completion"
