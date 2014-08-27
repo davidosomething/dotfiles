f798b0be491713d1b902332b349eae5aad2dc4dd
David O'Trakoun <david.o'trakoun@timeinc.com>
2014-08-27 10:19:51 -0400

    phpcs and file perms

--- php/phpcs.sh
+++ php/phpcs.sh
@@ -1,1 +1,5 @@
-phpcs --config-set installed_paths "/home/davidosomething/src/wpcs,/home/davidosomething/.composer/vendor/squizlabs/php_codesniffer/CodeSniffer"
+# on arch
+#phpcs --config-set installed_paths "/home/davidosomething/src/wpcs,/home/davidosomething/.composer/vendor/squizlabs/php_codesniffer/CodeSniffer"
+
+# on osx
+#phpcs --config-set installed_paths "/home/davidosomething/src/wpcs,/usr/local/Cellar/php-code-sniffer/1.5.4/CodeSniffer"
