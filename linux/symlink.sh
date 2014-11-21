c5df14f86c1f84deea82dd6eda479ba5af51f872
David O'Trakoun <david.o'trakoun@timeinc.com>
2014-11-21 15:14:50 -0500

    sym and rm nautilus accels since i use thunar

--- linux/symlink.sh
+++ linux/symlink.sh
@@ -1,7 +1,9 @@
 echo "Run these"
 
 cat <<EOF
-ln -sf ~/.dotfiles/linux/x/.xsession ~/.xsession
+ln -sf ~/.dotfiles/linux/x/.xprofile ~/.xprofile
+ln -sf ~/.dotfiles/linux/x/.xinitrc ~/.xsession
+ln -sf ~/.dotfiles/linux/x/.xinitrc ~/.xinitrc
 ln -sf ~/.dotfiles/linux/tint2/tint2rc ~/.config/tint2/tint2rc
 ln -sf ~/.dotfiles/linux/openbox/rc.xml ~/.config/openbox/rc.xml
 ln -sf ~/.dotfiles/linux/openbox/menu.xml ~/.config/openbox/menu.xml
