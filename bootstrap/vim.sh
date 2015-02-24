0907d8386778aaf67e92e1adfb66710fce92ba7f
David O'Trakoun <david.o'trakoun@timeinc.com>
2015-02-23 19:18:24 -0500

    don't need curl in vim

--- bootstrap/vim.sh
+++ bootstrap/vim.sh
@@ -12,8 +12,6 @@ dotfiles_path="$(pwd)"
 bootstrap_path="$dotfiles_path/bootstrap"
 source "$bootstrap_path/helpers.sh"
 
-dkorequire "curl"
-
 ################################################################################
 # Check if a vim folder already exists
 if [[ -d "$HOME/.vim" ]]; then
