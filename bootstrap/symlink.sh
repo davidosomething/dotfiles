b432aaa54830b893e2dc0c7f05e3802b1db06c69
David O'Trakoun <me@davidosomething.com>
2015-03-24 21:24:19 -0400

    subversion on linux cfg

--- bootstrap/symlink.sh
+++ bootstrap/symlink.sh
@@ -5,25 +5,31 @@ set -eu
 # Basic symlinks, safe to run on any system
 ################################################################################
 
-##
-# initialize script and dependencies
+# initialize script and dependencies -------------------------------------------
 # get this bootstrap folder
 cd "$(dirname "$0")"/..
 dotfiles_path="$(pwd)"
 bootstrap_path="$dotfiles_path/bootstrap"
 source "$bootstrap_path/helpers.sh"
 
-##
-# begin
+# begin ------------------------------------------------------------------------
 dkostatus "Symlinking dotfiles"
 dkosymlink ctags/.ctags.symlink       .ctags
 dkosymlink git/.gitconfig.symlink     .gitconfig
 dkosymlink mdl/.mdlrc.symlink         .mdlrc
 dkosymlink ruby/.gemrc.symlink        .gemrc
-dkosymlink subversion/config.symlink  .subversion/config
 
-##
-# symlink shells
+case "$OSTYPE" in
+  darwin*)
+    dkosymlink subversion/config.symlink  .subversion/config
+    ;;
+  linux*)
+    dkosymlink linux/subversion/config.symlink  .subversion/config
+    ;;
+esac
+
+
+# symlink shells ---------------------------------------------------------------
 dkosymlink bash/.bashrc.symlink         .bashrc
 dkosymlink bash/.bash_profile.symlink   .bash_profile
 dkosymlink zsh/.zshenv.symlink          .zshenv
