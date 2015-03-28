246d3f9a12930c360dc9bc0cc0d0c00a327eb86e
David O'Trakoun <me@davidosomething.com>
2015-03-27 20:41:56 -0400

    add instant-markdown-d to npm install

--- npm/install.sh
+++ npm/install.sh
@@ -6,6 +6,7 @@ npm install -g coffee-script
 npm install -g coffeelint
 npm install -g david
 npm install -g grunt-cli
+npm install -g instant-markdown-d
 npm install -g jscpd
 npm install -g jsctags
 npm install -g jshint
