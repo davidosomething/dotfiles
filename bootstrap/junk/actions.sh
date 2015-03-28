ced331fe561dc96d9231d9af056891b2b37b20ad
David O'Trakoun <me@davidosomething.com>
2015-03-28 00:33:51 -0400

    rm more junk

--- bootstrap/junk/actions.sh
+++ bootstrap/junk/actions.sh
@@ -13,13 +13,6 @@ done
 
 exit
 
-# determine if you installed and are using the alias or are running using oneliner
-if [ "$0" = "dotfiles" ]; then
-  dotfiles_scriptname="dotfiles"
-else
-  dotfiles_scriptname="./bootstrap.zsh"
-fi
-
 # save OS name
 case $OSTYPE in
   darwin*)  dotfiles_local_suffix="osx"
@@ -47,62 +40,6 @@ function dotfiles_setup_ssh_keys() {
     fi
   else
     status "found SSH keys for this user"
-  fi
-}
-
-function dotfiles_setup_git() {
-  echo "== setting up git == "
-  echo "You can run this again if you mess up. Leave blank to skip username/email fields."
-  echo -n "Please enter your full name: "; read fullname
-  # in case we skipped the email in the ssh section
-  if [ "$email" = "" ]; then
-    echo -n "Please enter your email: "; read email;
-  fi
-  [ "$fullname" != '' ] && git config --global user.name "$fullname"
-  [ "$email"    != '' ] && git config --global user.email "$email"
-
-  # auto color all the things!
-  git config --global color.ui auto
-
-  # vim as diff tool
-  git config --global diff.tool vimdiff
-
-  # set up browser for fugitive :Gbrowse
-  [ "$dotfiles_local_suffix" = "osx" ] && git config --global web.browser open
-
-  # a couple aliases
-  git config --global alias.b     branch
-  git config --global alias.co    checkout
-  git config --global alias.dc    "diff --cached"
-  git config --global alias.st    "status -bs"
-  git config --global alias.graph "log --graph --oneline"
-
-  status "global .gitconfig generated/updated!"
-}
-
-function dotfiles_setup_github() {
-  echo "== setting up github == "
-  echo "You can run this again if you mess up. Leave blank to skip a field."
-  echo -n "Please enter your github username:  "; read githubuser
-  echo -n "Please enter your github api token: "; read githubtoken
-  [ "$githubuser"  != '' ] && git config --global github.user "$githubuser"
-  [ "$githubtoken" != '' ] && git config --global github.token "$githubtoken"
-  status "global .gitconfig updated with github info"
-}
-
-function dotfiles_determine_github_write() {
-  echo "== github configuration =="
-
-  GITHUB_URL='git://github.com/davidosomething'
-
-  echo "Checking for existing .gitconfig with github token..."
-  if ! git config --global --get github.token >/dev/null 2>&1; then
-    echo '[WARNING] missing github token, disabling write access to repositories.'
-    echo '          This machine will not be able to push edits back to the repository!'
-  elif [ "`git config --global --get github.user`" = "davidosomething" ]; then
-    GITHUB_URL='git@github.com:davidosomething'
-    status  "github token found and github user is davidosomething."
-    status_ "If needed, your dotfiles will be cloned with write access."
   fi
 }
 
@@ -147,7 +84,6 @@ function dotfiles_usage() {
   echo "  -h | --help         show usage (you're looking at it)"
   echo "  -v | --version      show version info"
   echo "  -d | --debug        show debugging flags"
-  echo "  -V | --vim-plugins  list directories under vim/bundle/"
   echo "  --skip-dependency   don't check for dependencies"
   echo "  --skip-shell        don't check if zsh is the default shell"
   echo "  --skip-ssh          don't check for SSH keys"
@@ -168,7 +104,7 @@ function dotfiles_usage() {
   echo "setups:"
   echo "  By specifying a setup you will run only the ones specified."
   echo "  Valid setups to run include:"
-  echo "  bash, gitconfig, github, osx, pow, vim, zsh"
+  echo "  gitconfig, github, osx"
   echo
 }
 
@@ -185,9 +121,6 @@ while [ "$1" != "" ]; do
                           exit 0
                           ;;
     -d | --debug )        dotfiles_debug=1
-                          ;;
-    -V | --vim-plugins )  pushd ~/.dotfiles/vim/bundle && ls -d * && popd
-                          exit 0
                           ;;
     --skip-dependency )   dotfiles_skip_check_dependency=1
                           ;;
@@ -208,9 +141,6 @@ while [ "$1" != "" ]; do
     --all )               dotfiles_do_all=1
                           # see conditional section for all below
                           ;;
-    bash )                dotfiles_do_bash="y"
-                          dotfiles_do_ask=0
-                          ;;
     gitconfig )           dotfiles_do_gitconfig="y"
                           dotfiles_do_ask=0
                           ;;
@@ -218,15 +148,6 @@ while [ "$1" != "" ]; do
                           dotfiles_do_ask=0
                           ;;
     osx )                 dotfiles_do_osx="y"
-                          dotfiles_do_ask=0
-                          ;;
-    pow )                 dotfiles_do_pow="y"
-                          dotfiles_do_ask=0
-                          ;;
-    vim )                 dotfiles_do_vim="y"
-                          dotfiles_do_ask=0
-                          ;;
-    zsh )                 dotfiles_do_zsh="y"
                           dotfiles_do_ask=0
                           ;;
     * )                   echo
@@ -250,27 +171,19 @@ fi
 
 # --all overrides all setups
 if [ "$dotfiles_do_all" = 1 ]; then
-  dotfiles_do_bash="y"
   dotfiles_do_github="y"
   dotfiles_do_gitconfig="y"
   if [ "$dotfiles_local_suffix" = "osx" ]; then
     dotfiles_do_osx="y"
   fi
-  dotfiles_do_pow="y"
-  dotfiles_do_vim="y"
-  dotfiles_do_zsh="y"
   dotfiles_do_ask=0
 fi
 
 # --all overrides all setups
 if [ "$dotfiles_update_only" = 1 ]; then
-  dotfiles_do_bash="n"
   dotfiles_do_github="n"
   dotfiles_do_gitconfig="n"
   dotfiles_do_osx="n"
-  dotfiles_do_pow="n"
-  dotfiles_do_vim="n"
-  dotfiles_do_zsh="n"
   dotfiles_do_ask=0
 fi
 
@@ -278,13 +191,9 @@ if [ "$dotfiles_debug" = 1 ]; then
   echo "OSTYPE: $OSTYPE"
   echo "os: $dotfiles_local_suffix"
   echo "dotfiles_debug: $dotfiles_debug"
-  echo "dotfiles_do_bash: $dotfiles_do_bash"
   echo "dotfiles_do_github: $dotfiles_do_github"
   echo "dotfiles_do_gitconfig: $dotfiles_do_gitconfig"
   echo "dotfiles_do_osx: $dotfiles_do_osx"
-  echo "dotfiles_do_pow: $dotfiles_do_pow"
-  echo "dotfiles_do_vim: $dotfiles_do_vim"
-  echo "dotfiles_do_zsh: $dotfiles_do_zsh"
   echo "dotfiles_do_ask: $dotfiles_do_ask"
   echo "dotfiles_do_all: $dotfiles_do_all"
   echo "dotfiles_skip_check_dependency: $dotfiles_skip_check_dependency"
@@ -325,35 +234,10 @@ if [ "$dotfiles_do_ask" = 1 ]; then
 else
   status "Skipping action checks"
 fi
-if [ "$dotfiles_do_gitconfig:l" = "y" ]; then
-  dotfiles_setup_git
-else
-  status "Skipping gitconfig setup"
-fi
-if [ "$dotfiles_do_github:l" = "y" ]; then
-  dotfiles_setup_github
-else
-  status "Skipping github setup"
-fi
-
 if [ -d ~/.dotfiles ]; then
   status "folder ~/.dotfiles already exists, not cloning"
   dotfiles_update
-else
-  if [ "$dotfiles_skip_check_git_writable" != 1 ]; then
-    dotfiles_determine_github_write
-  fi
-  dotfiles_clone
 fi
-
-[ "$dotfiles_do_pow:l"  = "y" ] && dotfiles_symlink_powconfig
-
-[ "$dotfiles_do_vim:l" = "y" ] && dotfiles_symlink_vim
-[[ "$dotfiles_do_vim:l" = "y" || "$dotfiles_update_only" = "1" ]] && dotfiles_update_vim
-
-# do this last since it modifies paths
-[ "$dotfiles_do_bash:l" = "y" ] && dotfiles_symlink_bash
-[ "$dotfiles_do_zsh:l"  = "y" ] && dotfiles_symlink_zsh
 
 [ "$dotfiles_do_osx:l" = "y" ] && dotfiles_setup_osx
 
