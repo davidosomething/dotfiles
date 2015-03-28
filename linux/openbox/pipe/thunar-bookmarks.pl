4921356077dc41c3add8b24f0b5428197247e596
David O'Trakoun <me@davidosomething.com>
2015-03-28 12:21:24 -0400

    cleanup thunar bookmarks pipe menu

--- linux/openbox/pipe/thunar-bookmarks.pl
+++ linux/openbox/pipe/thunar-bookmarks.pl
@@ -1,11 +1,19 @@
 #!/usr/bin/env perl
+
+# openbox pipe menu: Open GTK Bookmarks in Thunar
+#
+# Original by dbbolton <danielbarrettbolton@gmail.com>
+# https://github.com/dbbolton/pipemenus/blob/master/thunar-bookmarks.pl
+#
+# Modified by davidosomething <me@davidosomething.com>
+# https://github.com/davidosomething/dotfiles/blob/master/linux/openbox/pipe/thunar-bookmarks.pl
+#
+
 use strict;
 use warnings;
 use autodie;
 use File::Basename;
-
-# dbbolton
-# danielbarrettbolton@gmail.com
+use URI::Escape;
 
 my $filemanager = "thunar";
 my $bookmarks_file = "$ENV{HOME}/.gtk-bookmarks";
@@ -18,53 +26,34 @@ chomp(@lines);
 print "<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n"
     . "<openbox_pipe_menu>\n"
 
-# Default bookmarks in thunar #########
+# Places #########
     . "<item label=\"".$ENV{USER}."\">\n"
-    . " <action name=\"Execute\">\n"
-    . "  <execute>\n"
-    . "   ".$filemanager." $ENV{HOME} \n"
-    . "  </execute>\n"
-    . " </action>\n"
-    . "</item>\n"
-
-    . "<item label=\"Trash\">\n"
-    . " <action name=\"Execute\">\n"
-    . "  <execute>\n"
-    . "   ".$filemanager." trash:// \n"
-    . "  </execute>\n"
-    . " </action>\n"
-    . "</item>\n"
-
-    . "<item label=\"Desktop\">\n"
-    . " <action name=\"Execute\">\n"
-    . "  <execute>\n"
-    . "   ".$filemanager." /home/$ENV{USER}/Desktop \n"
-    . "  </execute>\n"
-    . " </action>\n"
-    . "</item>\n"
-
-    . "<item label=\"File System\">\n"
-    . " <action name=\"Execute\">\n"
-    . "  <execute>\n"
-    . "   ".$filemanager." / \n"
-    . "  </execute>\n"
-    . " </action>\n"
+    . "<action name=\"Execute\">"
+    . "<execute>" . $filemanager . " $ENV{HOME}</execute>"
+    . "</action>"
     . "</item>\n"
 
     . "<separator />\n";
 
-# User-specified bookmarks ############
+# Bookmarks ############
 for (@lines) {
-    my $label = basename $_;
-    
-    print "<item label=\"".$label."\">\n"
-        . " <action name=\"Execute\">\n"
-        . "  <execute>\n"
-        . "   ".$filemanager." ".$_."\n"
-        . "  </execute>\n"
-        . " </action>\n"
-        . "</item>\n";
+    my $label = uri_unescape(basename $_);
+    print "<item label=\"". $label ."\">"
+        . "<action name=\"Execute\">"
+        . "<execute>" . $filemanager . " " . $_ . "</execute>"
+        . "</action>"
+        . "</item>";
 }
-print "</openbox_pipe_menu>\n";
+
+# Trash ############
+print "<separator />\n"
+
+    . "<item label=\"Trash\">\n"
+    . "<action name=\"Execute\">"
+    . "<execute>" . $filemanager . " trash://</execute>"
+    . "</action>"
+    . "</item>\n";
+
+print "</openbox_pipe_menu>";
 close $in;
 
