86c2da6ca419fe8e29f213ee6398bb35b910aac4
David O'Trakoun <me@davidosomething.com>
2015-05-06 23:04:27 -0400

    dog/cat

--- bootstrap/symlink.sh
+++ bootstrap/symlink.sh
@@ -12,7 +12,7 @@ source "$bootstrap_path/helpers.sh"
 
 # begin ------------------------------------------------------------------------
 dkostatus "Symlinking dotfiles"
-dkosymlink shell/.inputrc             .inputrc
+dkosymlink shell/.inputrc.symlink     .inputrc
 dkosymlink ctags/.ctags.symlink       .ctags
 dkosymlink git/.gitconfig.symlink     .gitconfig
 dkosymlink mdl/.mdlrc.symlink         .mdlrc
