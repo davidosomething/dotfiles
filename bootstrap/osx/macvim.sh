243052daae2d124b3ebe9afe1b27072c0d2a385d
David O'Trakoun <dotrakoun@timeinc.com>
2015-05-29 17:05:30 -0400

    upd macvim bootstrap

--- bootstrap/osx/macvim.sh
+++ bootstrap/osx/macvim.sh
@@ -6,12 +6,20 @@ brew uninstall --force macvim
 
 brew cleanup
 
+brew install ctags ctags-exuberant
+
+# NOTE:
+# Added --with-python explicitly even though formula options don't mention it.
+# --with-python and --with-python3 are not allowed together
+# See formula source instead:
+# https://github.com/Homebrew/homebrew/blob/master/Library/Formula/macvim.rb
+
 brew install \
   --build-from-source \
   --custom-icons \
   --override-system-vim \
   --with-lua \
-  --with-python3 \
+  --with-python \
   macvim
 
 brew linkapps macvim
--- osx/vim.sh
+++
@@ -1,7 +0,0 @@
-echo "Installing macvim using brew. System ruby is required, make sure you're using it."
-echo "Refer to https://github.com/Homebrew/homebrew/issues/23938#issuecomment-34774165"
-read -p "Press a key."
-brew install ctags ctags-exuberant
-brew install python3
-brew reinstall macvim --custom-icons --override-system-vim --with-cscope --with-lua
-brew linkapps
