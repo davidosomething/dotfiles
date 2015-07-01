f05e816bb93b490536ff881531bb50ee5fd2c938
David O'Trakoun <dotrakoun@timeinc.com>
2015-07-01 13:15:45 -0400

    php for osx good

---
+++ osx/Brewfiles/php.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env bash
+
+brew tap homebrew/homebrew-php
+brew update
+
+brew install php56
+brew install php56-imagick
+brew install php56-mcrypt
+brew install php56-memcache
+brew install php56-memcached
+brew install php56-mongo
+brew install php56-oauth
+brew install php56-tidy
+brew install php56-xdebug
+
+brew install composer
+# install phpmyadmin
+
+brew install phpcpd
+brew install phpmd
+brew install phpunit
+brew install wp-cli
+
