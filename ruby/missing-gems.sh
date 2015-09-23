f8dcf02b7f3edce327e43bf90a647c5637e42033
David O'Trakoun <dotrakoun@elitedaily.com>
2015-09-23 10:29:59 -0400

    add missing-gems.sh script

---
+++ ruby/missing-gems.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# loop through default-gems file and output if not installed
+while read gemname; do
+  gem list -i "$gemname" >/dev/null
+  if [ "$?" != "0" ]; then
+    echo "${gemname} not installed"
+  fi
+done < default-gems
+
