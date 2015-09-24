f432e7381a1909090e67a6964ec0e8128a9d8b26
David O'Trakoun <dotrakoun@elitedaily.com>
2015-09-24 12:41:12 -0400

    fix missing and install to global for npm

--- npm/install.sh
+++ npm/install.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 
 # loop through packages.txt file and install each one
-while read package; do npm install --global "$package"; done < packages.txt
+while read package; do
+  npm install -g "$package"
+done < packages.txt
 
--- npm/missing.sh
+++ npm/missing.sh
@@ -5,10 +5,13 @@ is_missing=0
 
 # loop through packages.txt file and output if not installed
 while read package; do
-  echo "$installed" | grep -q "$package"
+  package_name=$(echo "$package" | cut -f1 -d"@")
+  echo "$installed" | grep -q "$package_name"
   if [ "$?" != "0" ]; then
-    echo "${package} is not installed globally for this version of node"
+    echo "MISSING ${package_name} is not installed globally for this version of node"
     is_missing=1
+  else
+    echo "  FOUND ${package_name}"
   fi
 done < packages.txt
 
