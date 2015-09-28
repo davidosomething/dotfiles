7ab2771870bc23a7257b38749d21aa00c42340b6
David O'Trakoun <dotrakoun@elitedaily.com>
2015-09-28 12:51:53 -0400

    format merge prompt

--- zsh/prompt.zsh
+++ zsh/prompt.zsh
@@ -2,18 +2,38 @@
 setopt PROMPT_SUBST                   # allow variables in prompt
 autoload -U colors && colors
 
-# version control in prompt
+# enable version control in prompt
 autoload -Uz vcs_info
 precmd() { vcs_info; }
-zstyle ':vcs_info:*' enable bzr git svn
-zstyle ':vcs_info:git*' get-revision true
+zstyle ':vcs_info:*'            enable            bzr git hg svn
+
+# bzr/svn prompt
+zstyle ':vcs_info:(svn|bzr):*'  branchformat      'r%r'
+zstyle ':vcs_info:(svn|bzr):*'  formats           '(%b)'
+
+# git prompt (and git-svn)
+zstyle ':vcs_info:git*' get-revision      true
 zstyle ':vcs_info:git*' check-for-changes true
-zstyle ':vcs_info:git*' unstagedstr '*'  # display this when there are unstaged changes
-zstyle ':vcs_info:git*' stagedstr '+'    # display this when there are staged changes
-zstyle ':vcs_info:git*' formats '(%b%m%c%u)'
-zstyle ':vcs_info:git*' actionformats '(%b%m%c%u)[%a]'
-zstyle ':vcs_info:(svn|bzr):*' branchformat 'r%r'
-zstyle ':vcs_info:(svn|bzr):*' formats '(%b)'
+zstyle ':vcs_info:git*' unstagedstr       '*'
+zstyle ':vcs_info:git*' stagedstr         '+'
+zstyle ':vcs_info:git*' formats           '%F{magenta}(%b%c%u)'
+zstyle ':vcs_info:git*' actionformats     '%F{magenta}(%m %F{red}→%F{magenta} %b%c%u)'
+
+# use custom hook to parse merge message in actionformat
+zstyle ':vcs_info:git*+set-message:*' hooks gitmergemessage
+
+function +vi-gitmergemessage() {
+  if [ "${hook_com[action_orig]}" = "merge" ]; then
+    # misc_orig is in the format:
+    # bd69f0644eb9aa460da5de9ebf72e2e3c04b30f2 Merge branch 'x' (1 applied)
+
+    # get merge_from branch name
+    from=$(echo ${hook_com[misc]} | awk '{print $4}' | tr -d "'")
+
+    # modify %m
+    hook_com[misc]="${from}"
+  fi
+}
 
 # vi mode
 # http://paulgoscicki.com/archives/2012/09/vi-mode-indicator-in-zsh-prompt/
@@ -65,8 +85,8 @@ _set_prompt() {
 
   PROMPT='${prompt_user}%F{blue}@${prompt_host}%F{blue}:%F{yellow}%~'$'\n'
   PROMPT+='%f%*%F{blue}${vimode}'
-  PROMPT+='%F{magenta}${vcs_info_msg_0_}'
-  PROMPT+='%#%f '
+  PROMPT+='${vcs_info_msg_0_}'
+  PROMPT+='%F{blue}%#%f '
 }
 
 _set_prompt
