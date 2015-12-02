b014287f961836d9dff92e9853398a5ffbbd019c
David O'Trakoun <dotrakoun@elitedaily.com>
2015-12-02 11:12:05 -0500

    add rln, cleanup symlink, rename symlinks with .symlink suffix

--- bootstrap/symlink.sh
+++ bootstrap/symlink.sh
@@ -12,25 +12,25 @@ source "$bootstrap_path/helpers.sh"
 
 # begin ------------------------------------------------------------------------
 dkostatus "Symlinking dotfiles"
-dkosymlink aspell/.aspell.en.pws      .aspell.en.pws
-dkosymlink ctags/.ctags.symlink       .ctags
-dkosymlink mdl/.mdlrc.symlink         .mdlrc
-dkosymlink ruby/.gemrc.symlink        .gemrc
+dkosymlink aspell/.aspell.en.pws.symlink        .aspell.en.pws
+dkosymlink ctags/.ctags.symlink                 .ctags
+dkosymlink mdl/.mdlrc.symlink                   .mdlrc
+dkosymlink ruby/.gemrc.symlink                  .gemrc
 
 # XDG-compatible
-dkosymlink git/.gitconfig.symlink     .config/git/config
-dkosymlink shell/.inputrc.symlink     .config/readline/inputrc
+dkosymlink git/.gitconfig.symlink               .config/git/config
+dkosymlink shell/.inputrc.symlink               .config/readline/inputrc
 
 # irssi
-dkosymlink irssi  .irssi
+dkosymlink irssi                                .irssi
 
 # (n)vim
-dkosymlink vim  .vim
-dkosymlink vim  .config/nvim
+dkosymlink vim                                  .vim
+dkosymlink vim                                  .config/nvim
 
 case "$OSTYPE" in
   darwin*)
-    dkosymlink subversion/config.symlink  .subversion/config
+    dkosymlink subversion/config.symlink        .subversion/config
     ;;
   linux*)
     dkosymlink linux/subversion/config.symlink  .subversion/config
@@ -40,8 +40,8 @@ esac
 
 
 # symlink shells ---------------------------------------------------------------
-dkosymlink bash/.bashrc.symlink         .bashrc
-dkosymlink bash/.bash_profile.symlink   .bash_profile
-dkosymlink zsh/.zshenv.symlink          .zshenv
+dkosymlink bash/.bashrc.symlink                 .bashrc
+dkosymlink bash/.bash_profile.symlink           .bash_profile
+dkosymlink zsh/.zshenv.symlink                  .zshenv
 
 dkostatus "Done! [symlink.sh]"
