2de9403970ce7f67d55ec24f6a34e51fd8b71d49
David O'Trakoun <dotrakoun@elitedaily.com>
2015-12-08 10:58:50 -0500

    update update script and macvim reinstallation

--- bootstrap/osx/macvim.sh
+++ bootstrap/osx/macvim.sh
@@ -2,22 +2,12 @@
 
 set -eu
 
-brew uninstall --force macvim
-
-brew cleanup
-
 brew install ctags ctags-exuberant
 
-# NOTE:
-# Added --with-python explicitly even though formula options don't mention it.
-# --with-python and --with-python3 are not allowed together
-# See formula source instead:
-# https://github.com/Homebrew/homebrew/blob/master/Library/Formula/macvim.rb
-
-brew install \
+brew reinstall \
   --with-custom-icons \
   --with-lua \
-  --with-python \
+  --with-python3 \
   --with-override-system-vim \
   macvim
 
