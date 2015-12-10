4710bdb363164d764bfba2dddea49286b6e17c48
David O'Trakoun <me@davidosomething.com>
2015-12-09 20:43:09 -0500

    use herestring

--- node/missing.sh
+++ node/missing.sh
@@ -1,12 +1,12 @@
 #!/usr/bin/env bash
 
-installed=$(npm ls --global --parseable --depth=0)
+installed="$(npm ls --global --parseable --depth=0)"
 is_missing=0
 
 # loop through packages.txt file and output if not installed
 while read package; do
   package_name=$(echo "$package" | cut -f1 -d"@")
-  echo "$installed" | grep -q "$package_name"
+  grep -q "$package_name" <<< "$installed"
   if [ "$?" != "0" ]; then
     echo "MISSING ${package_name} is not installed globally for this version of node"
     is_missing=1
