6d102eddd4cdebec734c12a64a756220174cacaf
David O'Trakoun <me@davidosomething.com>
2016-01-16 18:39:15 -0500

    just install packages now

--- node/install.sh
+++ node/install.sh
@@ -4,6 +4,7 @@ set -eu
 
 # loop through packages.txt file and install each one
 while read -r package; do
-  npm ls --global --parseable --depth=0 "$package" || npm install --global "$package"
+  # npm ls --global --parseable --depth=0 "$package" ||
+  npm install --global "$package"
 done < packages.txt
 
