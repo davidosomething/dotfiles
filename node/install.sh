91d1ae8d712ddb363e73a7748d29d582afed328a
David O'Trakoun <me@davidosomething.com>
2016-01-16 19:24:05 -0500

    no exit on error

--- node/install.sh
+++ node/install.sh
@@ -1,10 +1,8 @@
 #!/usr/bin/env bash
 
-set -eu
-
 # loop through packages.txt file and install each one
 while read -r package; do
   # npm ls --global --parseable --depth=0 "$package" ||
-  npm install --global "$package"
+  npm install --force --global "$package"
 done < packages.txt
 
