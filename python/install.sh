d989f4ab7bf2e317040a4869e2263b49bae6e241
David O'Trakoun <me@davidosomething.com>
2016-03-07 19:22:24 -0500

    add uninstall all py packages, check for pyenv before installing pip pkgs

--- python/install.sh
+++ python/install.sh
@@ -1,8 +1,20 @@
 #!/usr/bin/env bash
 
 set -eu
-if [ -z "$DOTFILES" ]; then exit 1; fi
+if [ -z "$DOTFILES" ]; then
+  echo ".dotfiles repo is not set up"
+  exit 1
+fi
 source "${DOTFILES}/bootstrap/helpers.sh"
+
+if pip --version | grep -q /usr/lib; then
+  dkoerr "System pip detected, not running."
+  dkoerr_ "Set up a global pyenv instead."
+  exit 1
+fi
+
+dkostatus "Updating global pip"
+pip install --upgrade pip
 
 dkostatus "Updating global pip requirements"
 pip install --upgrade --requirement "${DOTFILES}/python/requirements.txt"
---
+++ python/uninstall-all.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+
+pip freeze > unins && pip uninstall -y -r unins && rm unins
+
