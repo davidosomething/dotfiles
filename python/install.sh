e5b4a3d8e37fa77f762b7c1b12632d78474cdce2
David O'Trakoun <dotrakoun@elitedaily.com>
2016-06-28 10:15:27 -0400

    cleanup python pip install

--- python/install.sh
+++ python/install.sh
@@ -1,15 +1,21 @@
 #!/usr/bin/env bash
 
 set -eu
+
 if [ -z "$DOTFILES" ]; then
   echo ".dotfiles repo is not set up"
   exit 1
 fi
 source "${DOTFILES}/bootstrap/helpers.sh"
 
+if ! has_program "pyenv"; then
+  dkoerr  "pyenv is not installed. Install it and set up a global pyenv."
+  exit 1
+fi
+
+# Make sure not using macOS internal python and pip
 if pip --version | grep -q /usr/lib; then
-  dkoerr "System pip detected, not running."
-  dkoerr_ "Set up a global pyenv instead."
+  dkoerr  "System pip detected, not running. Use a userspace python's pip."
   exit 1
 fi
 
