76674ccda1b884a44bb0c11f81df4d5e275ce245
David O'Trakoun <dotrakoun@elitedaily.com>
2016-06-28 10:21:52 -0400

    remove php56

--- bootstrap/mac/php.sh
+++ bootstrap/mac/php.sh
@@ -3,19 +3,9 @@
 brew tap homebrew/homebrew-php
 brew update
 
-brew install php56
-brew install php56-imagick
-brew install php56-mcrypt
-brew install php56-memcache
-brew install php56-memcached
-brew install php56-mongo
-brew install php56-oauth
-brew install php56-tidy
-brew install php56-xdebug
+# phpbrew?
 
 brew install composer
-# install phpmyadmin
-
 brew install phpcpd
 brew install phpmd
 brew install phpunit
