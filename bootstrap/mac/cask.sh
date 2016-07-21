f454dfef58bacaeac85209e032c27b931959d869
David O'Trakoun <me@davidosomething.com>
2016-07-20 21:59:46 -0400

    cask info

--- bootstrap/mac/cask.sh
+++ bootstrap/mac/cask.sh
@@ -5,27 +5,73 @@
 brew tap caskroom/cask
 brew update
 
+# ==============================================================================
+# Alfred
+# ==============================================================================
+
 brew cask install alfred
 brew cask alfred link
+echo "License in email"
+echo "Workflows in Dropbox"
 
-brew cask install android-file-transfer
+# ==============================================================================
+# bettertouchtool
+# ==============================================================================
+
 brew cask install bettertouchtool
+echo "License in secrets"
 
-brew cask install charles
-brew cask install firefox
+# ==============================================================================
 
 # monitor git repos in menubar
 #brew cask install gitifier
 
+brew cask install flux
+brew cask install gpgtools
+brew cask install hyperterm
+brew cask install java
 brew cask install jing
 brew cask install joinme
 
+# ==============================================================================
+# Kaleidoscope
+# ==============================================================================
+
+brew cask install kaleidoscope
+echo "License in secrets"
+
+# ==============================================================================
+# karabiner
+# ==============================================================================
+
+brew cask install karabiner
+echo "Exports in mac/karabiner"
+
+# ==============================================================================
+# Marked
+# ==============================================================================
+
 brew cask install marked
+echo "License in email"
 
-# don't use reverse scrolling on a mouse
+# ==============================================================================
+
 brew cask install skype
-brew cask install transmit
+brew cask install slack
+brew cask install spideroakone
+brew cask install spotify spotifree
+brew cask install steam
+brew cask install transmission
 
-brew cask install vagrant-manager
+# ==============================================================================
+# Transmit
+# ==============================================================================
+
+brew cask install transmit
+echo "License in email"
+echo "Favorites in Dropbox"
+
+brew cask install vagrant vagrant-manager
 brew cask install virtualbox virtualbox-extension-pack
 brew cask install vlc
+brew cask install vnc-viewer
