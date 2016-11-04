1e0f1f1980f5783c78e7e40f22785b2e95ad624a
David O'Trakoun <dotrakoun@elitedaily.com>
2016-11-04 18:26:51 -0400

    bindkeys tweaking

--- zsh/keybindings.zsh
+++ zsh/keybindings.zsh
@@ -1,4 +1,9 @@
 # zsh/keybindings.zsh
+#
+# These keys should also be set in shell/.inputrc
+#
+# `cat -e` to test out keys
+#
 
 export DKO_SOURCE="${DKO_SOURCE} -> keybindings.zsh"
 
@@ -8,33 +13,57 @@ unsetopt flowcontrol
 # VI mode
 bindkey -v
 
-# These keys should also be set in shell/.inputrc
 # search through history starting with current buffer contents
-bindkey "^[[A"  history-beginning-search-backward
-bindkey "^[[B"  history-beginning-search-forward
-# fix backspace
-bindkey '^?'    backward-delete-char
-# fix delete
-bindkey '^[[3~' delete-char
-# fix up and down to end of line after history
-bindkey '\e[A'  history-search-backward  # Up
-bindkey '\e[B'  history-search-forward   # Down
-# option+ left and right should jump through words
-bindkey '\e\e[C' forward-word            # Right
-bindkey '\e\e[D' backward-word           # Left
-# ctrl-left/right word
-bindkey '^[[1;5C' forward-word
-bindkey '\e[1;5C' forward-word
-bindkey '^[[1;5D' backward-word
-bindkey '\e[1;5D' backward-word
-# home/end
-bindkey '^[[H'  beginning-of-line
-bindkey '^[[1~' beginning-of-line
-bindkey '^[OH'  beginning-of-line
-bindkey '^[[F'  end-of-line
-bindkey '^[[4~' end-of-line
-bindkey '^[OF'  end-of-line
-# VI mode allow home and end
-bindkey -M vicmd '^[[H' beginning-of-line
-bindkey -M vicmd '^[[F' end-of-line
+# bindkey "\e\eA"  history-beginning-search-backward
+# bindkey "\e\eB"  history-beginning-search-forward
 
+# fix backspace
+# bindkey '^?'    backward-delete-char
+
+# fix up and down to end of line after history
+# bindkey '\e[A'  history-search-backward  # Up
+# bindkey '\e[B'  history-search-forward   # Down
+
+# home/end
+# Home/Fn-Left
+bindkey '^[[H'  beginning-of-line
+# bindkey '^[[1~' beginning-of-line
+# bindkey '^[OH'  beginning-of-line
+
+# End/Fn-Right
+bindkey '^[[F'  end-of-line
+# bindkey '^[[4~' end-of-line
+# bindkey '^[OF'  end-of-line
+
+# ============================================================================
+# macbook, iterm2, xterm-256color-italic, zsh
+# ============================================================================
+
+# Home/Fn-Left
+bindkey           '^[[H'  beginning-of-line
+bindkey -M vicmd  '^[[H'  beginning-of-line
+
+# End/Fn-Right
+bindkey           '^[[F'  end-of-line
+bindkey -M vicmd  '^[[F'  end-of-line
+
+# fix delete - Fn-delete
+bindkey '^[[3~' delete-char
+
+# Left and right should jump through words
+# Opt-Left - Same as ^[^[[D
+bindkey '\e\e[D' backward-word
+# Opt-Right - Same as ^[^[[C
+bindkey '\e\e[C' forward-word
+# C-L
+bindkey '^[[1;5D' backward-word
+#bindkey '\e[1;5D' backward-word
+# C-R
+bindkey '^[[1;5C' forward-word
+#bindkey '\e[1;5C' forward-word
+
+# PgUp/Dn navigate through history like regular up/down
+bindkey -M viins "^[[5~" up-history
+bindkey -M vicmd "^[[5~" up-history
+bindkey -M viins "^[[6~" down-history
+bindkey -M vicmd "^[[6~" down-history
