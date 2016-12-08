8dc8a990149899521e9a60b3bbc613cade6af194
David O'Trakoun <dotrakoun@elitedaily.com>
2016-12-08 13:22:38 -0500

    remove dupe completions

--- zsh/zplug.zsh
+++ zsh/zplug.zsh
@@ -5,7 +5,7 @@
 # Use repo format for oh-my-zsh plugins so no random crap is sourced
 #
 # Make sure fpaths are defined before or within zplug -- it calls compinit
-# again in between loading plugins and nice plugins.
+# again in between loading plugins and defer plugins.
 #
 
 # ----------------------------------------------------------------------------
@@ -39,51 +39,37 @@ zplug "plugins/colored-man-pages", from:oh-my-zsh
 # In-line best history match suggestion
 zplug "tarruda/zsh-autosuggestions"
 
-# Various program completions
+# Various program completions; includes go, nvm
 # This adds to fpath (so before compinit)
 zplug "zsh-users/zsh-completions"
 
 # ----------------------------------------------------------------------------
-# LAST, these call "compdef" so must be run after compinit, enforced by nice
+# LAST, these call "compdef" so must be run after compinit
 # ----------------------------------------------------------------------------
 
-# fork of rupa/z with better completion (so needs nice)
+# fork of rupa/z with better completion (so needs defer)
 zplug "knu/z",  \
   use:"z.sh",   \
-  nice:10
-
-# ----------------------------------------------------------------------------
-# Completions that require compdef (so nice 10)
-# ----------------------------------------------------------------------------
+  defer:2
 
 # gulp completion (parses file so not 100% accurate)
-zplug "akoenig/gulp.plugin.zsh", nice:10
-
-zplug "plugins/golang", \
-  from:oh-my-zsh,       \
-  nice:10
+zplug "akoenig/gulp.plugin.zsh", defer:2
 
 zplug "lukechilds/zsh-better-npm-completion", \
-  nice:11
-
-# 2016-04-27 nvm assumes ~/.nvm exists, so probably not working
-# My (unmerged) PR here:
-#   https://github.com/robbyrussell/oh-my-zsh/pull/5047/files
-# zplug "robbyrussell/oh-my-zsh", \
-#   use:"plugins/nvm/_nvm",       \
-#   nice:10
+  defer:2
 
 # Moved if block outside since `zplug check` doesn't consider it
 if [[ $OSTYPE == "darwin"* ]]; then
   zplug "vasyharan/zsh-brew-services",  \
-    nice:10
+    defer:2
 fi
 
 # # completions I have locally
 # zplug "${ZDOTDIR}",     \
 #   from:local,           \
 #   use:"completions/_*", \
-#   nice:18
+#   defer:18
 
+# absolute last
 # highlight as you type
-zplug "zsh-users/zsh-syntax-highlighting", nice:19
+zplug "zsh-users/zsh-syntax-highlighting", defer:3
