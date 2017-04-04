4ee1ee1827a74157fe8a96156fcdb56c097b6df8
David O'Trakoun <dotrakoun@elitedaily.com>
2017-04-04 15:04:50 -0400

    add java bin to path

--- shell/java.bash
+++ shell/java.bash
@@ -2,4 +2,7 @@
 
 dko::has 'javac' \
   && JAVA_HOME="$(dirname "$(readlink "$(which javac)")")/java_home"
-export JAVA_HOME
+
+[ -z "$JAVA_HOME" ] \
+  && export JAVA_HOME \
+  && export PATH="${JAVA_HOME}/bin:${PATH}"
