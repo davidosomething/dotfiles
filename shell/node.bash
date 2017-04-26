aacd4b0e12797b2b516f27551a0a969992d1ae0f
David O'Trakoun <me@davidosomething.com>
2017-04-26 10:29:22 -0400

    use env vars for npm overrides since nvm may not respect userconfig

--- shell/node.bash
+++ shell/node.bash
@@ -1,6 +1,20 @@
 # shell/node.bash
 export DKO_SOURCE="${DKO_SOURCE} -> shell/node.bash {"
 
+# ============================================================================
+# npm config
+# ============================================================================
+
+[ -f "${DOTFILES}/local/npmrc" ] \
+  && export NPM_CONF_USERCONFIG="${DOTFILES}/local/npmrc"
+
+export NPM_CONFIG_INIT_VERSION="0.0.1"
+export NPM_CONFIG_INIT_LICENSE="MIT"
+export NPM_CONFIG_STRICT_SSL="TRUE"
+export NPM_CONFIG_MESSAGE="Cut %s (via npm version)"
+
+# more user overrides in ~/.dotfiles/local/{bash,zsh}rc
+
 # ==============================================================================
 # nvm
 # ==============================================================================
@@ -20,6 +34,10 @@ dko::source "${NVM_DIR}/nvm.sh" && {
   DKO_DEFAULT_NODE_VERSION="$(nvm version default)"
   export DKO_DEFAULT_NODE_VERSION
 }
+
+# ============================================================================
+# npm global packages
+# ============================================================================
 
 dko::has 'trash' && alias rm=trash
 
