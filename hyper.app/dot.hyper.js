6227cea585af9a1bec008618659f7bb8278477d5
David O'Trakoun <dotrakoun@elitedaily.com>
2017-05-04 12:18:00 -0400

    bump hyper font size

--- hyper.app/dot.hyper.js
+++ hyper.app/dot.hyper.js
@@ -1,7 +1,7 @@
 module.exports = {
   config: {
     // default font size in pixels for all tabs
-    fontSize: 12,
+    fontSize: 16,
 
     // font family with optional fallbacks
     fontFamily: '"Fira Mono for Powerline", Menlo, "DejaVu Sans Mono", "Lucida Console", monospace',
