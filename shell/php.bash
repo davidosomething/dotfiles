44a7c8df3d6c99aa269c0a3f9b7378f777c63bf5
David O'Trakoun <dotrakoun@elitedaily.com>
2017-05-05 15:02:48 -0400

    remove php utility fns, use php-config --xyz instead

--- shell/php.bash
+++ shell/php.bash
@@ -1,26 +1,12 @@
 # shell/php.bash
 export DKO_SOURCE="${DKO_SOURCE} -> shell/php.bash {"
 
-# ==============================================================================
-# utility
-# ==============================================================================
+# ============================================================================
+# php-version
+# ============================================================================
 
-# PHP version numbers
-phpver() {
-  php -d display_startup_errors=0 \
-    -r 'echo phpversion();' \
-    2>/dev/null
-}
-
-phpminorver() {
-  php -d display_startup_errors=0 \
-    -r "echo PHP_MAJOR_VERSION . '.' . PHP_MINOR_VERSION;" \
-    2>/dev/null
-}
-
-phpconfdir() {
-  phpini="$(php -d display_startup_errors=0 -r 'echo php_ini_loaded_file();' 2>/dev/null)"
-  dirname "$phpini"
+dko::source "${DKO_BREW_PREFIX}/opt/php-version/php-version.sh" && {
+  php-version 7.1
 }
 
 # ==============================================================================
