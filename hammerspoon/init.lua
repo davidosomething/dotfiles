49581525151e2e8c06036b23af5d6244bce6b2ac
David O'Trakoun <me@davidosomething.com>
2017-05-22 23:51:41 -0400

    require shift for manual paste

--- hammerspoon/init.lua
+++ hammerspoon/init.lua
@@ -4,6 +4,6 @@
 
 ---
 -- cmd-alt-v to type what is in the clipboard
-hs.hotkey.bind({"cmd", "alt"}, "V", function()
+hs.hotkey.bind({"cmd", "alt", "shift"}, "V", function()
   hs.eventtap.keyStrokes(hs.pasteboard.getContents())
 end)
