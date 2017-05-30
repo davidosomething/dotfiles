db51c0687747aecfbfe998fd0f9bcedfdc2a7311
David O'Trakoun <dotrakoun@elitedaily.com>
2017-05-30 16:55:13 -0400

    single quote aliases that don't need expansion

--- shell/aliases.bash
+++ shell/aliases.bash
@@ -15,48 +15,48 @@ alias rm='rm -i'
 # paths and dirs
 # ----------------------------------------------------------------------------
 
-alias ..="cd .."
-alias ....="cd ../.."
-alias cd..="cd .."
-alias cdd="cd \"\${DOTFILES}\""
-alias cdv="cd \"\${VDOTDIR}\""
-alias dirs="dirs -v"                  # default to vert, use -l for list
-alias tree="tree -CF"
+alias ..='cd ..'
+alias ....='cd ../..'
+alias cd..='cd ..'
+alias cdd='cd "${DOTFILES}"'
+alias cdv='cd "${VDOTDIR}"'
+alias dirs='dirs -v'                  # default to vert, use -l for list
+alias tree='tree -CF'
 
 # ----------------------------------------------------------------------------
 # cat (prefer bin/dog)
 # ----------------------------------------------------------------------------
 
-alias pyg="pygmentize -O style=rrt -f console256 -g"
+alias pyg='pygmentize -O style=rrt -f console256 -g'
 
 # ----------------------------------------------------------------------------
 # editors
 # ----------------------------------------------------------------------------
 
-alias a="atom-beta"
-alias e="vim"
-alias ehosts="se /etc/hosts"
-alias etmux="e \"\${DOTFILES}/tmux/tmux.conf\""
-alias evr="e \"\${VDOTDIR}/vimrc\""
-alias eze="e \"\${ZDOTDIR}/.zshenv\""
-alias ezp="e \"\${ZDOTDIR}/zplug.zsh\""
-alias ezr="e \"\${ZDOTDIR}/.zshrc\""
+alias a='atom-beta'
+alias e='vim'
+alias ehosts='se /etc/hosts'
+alias etmux='e "${DOTFILES}/tmux/tmux.conf"'
+alias evr='e "${VDOTDIR}/vimrc"'
+alias eze='e "${ZDOTDIR}/.zshenv"'
+alias ezp='e "${ZDOTDIR}/zplug.zsh"'
+alias ezr='e "${ZDOTDIR}/.zshrc"'
 
 # ----------------------------------------------------------------------------
 # git
 # ----------------------------------------------------------------------------
 
-if command -v hub >/dev/null; then alias g="hub"; else alias g="git"; fi
-alias g-="g checkout -"
-alias gb="g branch --verbose"
-alias gi="g ink"
-alias gg="g grep --line-number --break --heading"
-alias gl="g l --max-count 25"
-alias gm="g checkout master"
-alias gp="g push"
-alias gpo="g push origin"
-alias gs="g status"
-alias gt="g take"
+if command -v hub >/dev/null; then alias g='hub'; else alias g='git'; fi
+alias g-='g checkout -'
+alias gb='g branch --verbose'
+alias gi='g ink'
+alias gg='g grep --line-number --break --heading'
+alias gl='g l --max-count 25'
+alias gm='g checkout master'
+alias gp='g push'
+alias gpo='g push origin'
+alias gs='g status'
+alias gt='g take'
 
 # ----------------------------------------------------------------------------
 # greppers
@@ -78,43 +78,43 @@ alias rg='rg --hidden               --line-number  --no-ignore-vcs     --smart-c
 # node
 # ----------------------------------------------------------------------------
 
-alias n="npm"
-alias ni="n install"
-alias no="n outdated --long"
-alias nr="n run --silent"
-alias ns="n start"
-alias nt="n test"
-alias nu="n update"
-alias nude="nvm use default"
-alias sme="source-map-explorer"
-alias y="yarn"
-alias yi="yarn install"
-alias yr="yarn run"
+alias n='npm'
+alias ni='n install'
+alias no='n outdated --long'
+alias nr='n run --silent'
+alias ns='n start'
+alias nt='n test'
+alias nu='n update'
+alias nude='nvm use default'
+alias sme='source-map-explorer'
+alias y='yarn'
+alias yi='yarn install'
+alias yr='yarn run'
 
 # ----------------------------------------------------------------------------
 # php
 # ----------------------------------------------------------------------------
 
-alias cm="composer"
+alias cm='composer'
 
 # ----------------------------------------------------------------------------
 # python
 # ----------------------------------------------------------------------------
 
-alias pea="pyenv activate"
-alias ped="pyenv deactivate"
-alias pss="pyenv shell system"
-alias py2="python2"
-alias py3="python3"
-alias py="python"
+alias pea='pyenv activate'
+alias ped='pyenv deactivate'
+alias pss='pyenv shell system'
+alias py2='python2'
+alias py3='python3'
+alias py='python'
 
 # ----------------------------------------------------------------------------
 # ruby
 # ----------------------------------------------------------------------------
 
-alias bun="bundle"
-alias be="bun exec"
-alias cap="be cap"
+alias bun='bundle'
+alias be='bun exec'
+alias cap='be cap'
 
 # ----------------------------------------------------------------------------
 # ssh keys
@@ -130,73 +130,73 @@ alias sshkeygen='ssh-keygen -o -a 100 -t ed25519'
 # sudo ops
 # ----------------------------------------------------------------------------
 
-alias mine="sudo chown -R \"\$USER\""
-alias root="sudo -s"
-alias se="sudo -e"
+alias mine='sudo chown -R "$USER"'
+alias root='sudo -s'
+alias se='sudo -e'
 
 # ----------------------------------------------------------------------------
 # tmux
 # ----------------------------------------------------------------------------
 
-alias tmux="tmux -f \"\${DOTFILES}/tmux/tmux.conf\""
-alias ta="tmux attach"
+alias tmux='tmux -f "${DOTFILES}/tmux/tmux.conf"'
+alias ta='tmux attach'
 
 # ----------------------------------------------------------------------------
 # xcode
 # ----------------------------------------------------------------------------
 
-alias cuios="XCODE_XCCONFIG_FILE=\"\${PWD}/xcconfigs/swift31.xcconfig carthage update --platform iOS"
+alias cuios='XCODE_XCCONFIG_FILE="${PWD}/xcconfigs/swift31.xcconfig" carthage update --platform iOS'
 
 # ----------------------------------------------------------------------------
 # rest of bins
 # ----------------------------------------------------------------------------
 
-alias archey="archey --offline"
-alias cb="cdbk"
-alias curl="curl --config \"\${DOTFILES}/curl/dot.curlrc\""
-alias df="df -h"
-alias ln="ln -v"
-alias mdl="mdl --config \"\${DOTFILES}/mdl/dot.mdlrc\""
-alias neofetch="neofetch --image ~/Dropbox/_avatars/trafalgarlaw_W.png --size 240px --gap 20 --disable de wm"
+alias archey='archey --offline'
+alias cb='cdbk'
+alias curl='curl --config "${DOTFILES}/curl/dot.curlrc"'
+alias df='df -h'
+alias ln='ln -v'
+alias mdl='mdl --config "${DOTFILES}/mdl/dot.mdlrc"'
+alias neofetch='neofetch --image ~/Dropbox/_avatars/trafalgarlaw_W.png --size 240px --gap 20 --disable de wm'
 alias o='dko-open'
 alias publicip='\curl icanhazip.com'
 alias rsync='rsync --human-readable --partial --progress'
 alias t="tree -a --noreport --dirsfirst -I '.git|node_modules|bower_components|.DS_Store'"
 alias today='date +%Y-%m-%d'
-alias tpr="tput reset"                # really clear the scrollback
-alias u="dko::dotfiles"
-alias vag="vagrant"
-alias vb="VBoxManage"
-alias vbm="vb"
-alias weechat="weechat -d \"\${DOTFILES}/weechat\""
-alias wget="wget --no-check-certificate --hsts-file=\"\${XDG_DATA_HOME}/wget/.wget-hsts\""
-alias xit="exit" # dammit
+alias tpr='tput reset'                # really clear the scrollback
+alias u='dko::dotfiles'
+alias vag='vagrant'
+alias vb='VBoxManage'
+alias vbm='vb'
+alias weechat='weechat -d "${DOTFILES}/weechat"'
+alias wget='wget --no-check-certificate --hsts-file="${XDG_DATA_HOME}/wget/.wget-hsts"'
+alias xit='exit' # dammit
 
 # ============================================================================
 
 __alias_ls() {
-  local almost_all="-A" # switchted from --almost-all for old bash support
-  local classify="-F" # switched from --classify for old bash support
-  local colorized="--color=auto"
-  local groupdirs="--group-directories-first"
-  local literal=""
-  local long="-l"
-  local single_column="-1"
-  local timestyle=""
+  local almost_all='-A' # switchted from --almost-all for old bash support
+  local classify='-F' # switched from --classify for old bash support
+  local colorized='--color=auto'
+  local groupdirs='--group-directories-first'
+  local literal=''
+  local long='-l'
+  local single_column='-1'
+  local timestyle=''
 
   if ! ls $groupdirs >/dev/null 2>&1; then
-    groupdirs=""
+    groupdirs=''
   fi
 
-  if [ "$DOTFILES_OS" = "Darwin" ]; then
-    almost_all="-A"
-    classify="-F"
-    colorized="-G"
+  if [ "$DOTFILES_OS" = 'Darwin' ]; then
+    almost_all='-A'
+    classify='-F'
+    colorized='-G'
   fi
 
-  if [ "$DOTFILES_OS" = "Linux" ] && [ "$DOTFILES_DISTRO" != "busybox" ]; then
-    literal="-N"
-    timestyle="--time-style=\"+%Y%m%d\""
+  if [ "$DOTFILES_OS" = 'Linux' ] && [ "$DOTFILES_DISTRO" != 'busybox' ]; then
+    literal='-N'
+    timestyle='--time-style="+%Y%m%d"'
   fi
 
   # shellcheck disable=SC2139
@@ -217,34 +217,34 @@ __alias_exa() {
     return
   fi
 
-  alias exa="exa --long --all --group-directories-first --group --git"
+  alias exa='exa --long --all --group-directories-first --group --git'
 }
 __alias_exa
 
 # ============================================================================
 
 __alias_darwin() {
-  alias b="TERM=xterm-256color brew"
-  alias brew="TERM=xterm-256color brew"
+  alias b='TERM=xterm-256color brew'
+  alias brew='b'
 
-  alias bi="b install"
-  alias bq="b list"
-  alias bs="b search"
+  alias bi='b install'
+  alias bq='b list'
+  alias bs='b search'
 
-  alias bsvc="b services"
-  alias bsvr="b services restart"
+  alias bsvc='b services'
+  alias bsvr='b services restart'
 
-  alias redstart="bsvc start redshift"
-  alias redstop="bsvc stop redshift"
+  alias redstart='bsvc start redshift'
+  alias redstop='bsvc stop redshift'
 
   # sudo since we run nginx on port 80 so needs admin
-  alias rnginx="sudo brew services restart nginx"
+  alias rnginx='sudo brew services restart nginx'
 
   # electron apps can't focus if started using Electron symlink
-  alias elec="/Applications/Electron.app/Contents/MacOS/Electron"
+  alias elec='/Applications/Electron.app/Contents/MacOS/Electron'
 
   # clear xattrs
-  alias xc="xattr -c *"
+  alias xc='xattr -c *'
 
   # Audio control - http://xkcd.com/530/
   alias stfu="osascript -e 'set volume output muted true'"
@@ -253,8 +253,8 @@ __alias_darwin() {
 # ============================================================================
 
 __alias_linux() {
-  alias open="o"                    # use open() function in shell/functions
-  alias startx="startx > \"\${DOTFILES}/logs/startx.log\" 2>&1"
+  alias open='o'                    # use open() function in shell/functions
+  alias startx='startx > "${DOTFILES}/logs/startx.log" 2>&1'
 
   alias testnotification="notify-send \
     'Hello world!' \
@@ -268,33 +268,33 @@ __alias_arch() {
   alias paclast="expac --timefmt='%Y-%m-%d %T' '%l\t%n' | sort | tail -20"
 
   if command -v pacaur >/dev/null; then
-    alias b="pacaur"
+    alias b='pacaur'
   elif command -v yaourt >/dev/null; then
-    alias b="yaourt"
+    alias b='yaourt'
   fi
-  alias bi="b -S"
-  alias bq="b -Qs"
-  alias bs="b -Ss"
+  alias bi='b -S'
+  alias bq='b -Qs'
+  alias bs='b -Ss'
 
   # Always create log file
-  alias makepkg="makepkg --log"
+  alias makepkg='makepkg --log'
 
-  alias rphp="sudo systemctl restart php-fpm.service"
-  alias rnginx="sudo systemctl restart nginx.service"
+  alias rphp='sudo systemctl restart php-fpm.service'
+  alias rnginx='sudo systemctl restart nginx.service'
 }
 
 # ============================================================================
 
 __alias_deb() {
-  alias b="sudo apt"
-  alias bi="b install"
-  alias bs="b search"
+  alias b='sudo apt'
+  alias bi='b install'
+  alias bs='b search'
 }
 
 # ============================================================================
 
 __alias_fedora() {
-  alias b="sudo dnf"
+  alias b='sudo dnf'
 }
 
 # ============================================================================
