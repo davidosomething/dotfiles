3810011c94f18ae1a432c65dc5204648be9ad21d
David O'Trakoun <me@davidosomething.com>
2017-07-26 20:59:01 -0400

    add LS_COLORS

--- shell/vars.bash
+++ shell/vars.bash
@@ -113,6 +113,10 @@ export LESS="-eFRX"
 # disable less history
 export LESSHISTFILE=-
 
+# custom LS_COLORS for deb, might not want on all machines
+# @TODO
+export LS_COLORS="no=00:fi=00:di=00;34:ln=00;36:pi=40;33:so=00;35:bd=40;33;01:cd=40;33;01:or=01;05;37;41:mi=01;05;37;41:ex=00;32:*.cmd=00;32:*.exe=00;32:*.com=00;32:*.btm=00;32:*.bat=00;32:*.sh=00;32:*.csh=00;32:*.tar=00;31:*.tgz=00;31:*.arj=00;31:*.taz=00;31:*.lzh=00;31:*.zip=00;31:*.z=00;31:*.Z=00;31:*.gz=00;31:*.bz2=00;31:*.bz=00;31:*.tz=00;31:*.rpm=00;31:*.cpio=00;31:*.jpg=00;35:*.gif=00;35:*.bmp=00;35:*.xbm=00;35:*.xpm=00;35:*.png=00;35:*.tif=00;35:"
+
 # man
 export MANWIDTH=88
 export MANPAGER="$PAGER"
