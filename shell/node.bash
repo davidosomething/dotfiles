9d3ffb36ba19b1af50465985c22a83dc652989be
David O'Trakoun <me@davidosomething.com>
2017-07-31 23:07:28 -0400

    faster prompt for nvm_ls

--- shell/node.bash
+++ shell/node.bash
@@ -29,6 +29,7 @@ dko::source "${NVM_DIR}/nvm.sh" && {
   # Get initial nvm version using bash string manipulation instead of NVM
   # calls. While this is significantly faster, it is not correct if starting
   # a shell in a dir with a .nvmrc ~= default (which I almost never do).
+  # Also does not use vX.X.X -- just X.X.X
   DKO_DEFAULT_NODE_VERSION="${${NVM_BIN/$NVM_DIR\/versions\/node\/v}%\/b*}"
   #DKO_DEFAULT_NODE_VERSION="$(nvm version default)"
   export DKO_DEFAULT_NODE_VERSION
--- zsh/prompt.zsh
+++ zsh/prompt.zsh
@@ -43,10 +43,13 @@ __dko_prompt_left_parts+=('%~')
 __dko_prompt_right_colors=()
 __dko_prompt_right_parts=()
 dko::has "nvm" && {
+  __current_node="${${NVM_BIN/$NVM_DIR\/versions\/node\/v}%\/b*}"
   __dko_prompt_right_colors+=('%F{blue}')
   __dko_prompt_right_parts+=('js:')
-  __dko_prompt_right_colors+=('$( [ "${(e)$(nvm_ls current 2>/dev/null)}" = "$DKO_DEFAULT_NODE_VERSION" ] && echo "%F{blue}" || echo "%F{red}")')
-  __dko_prompt_right_parts+=('${$(nvm_ls current 2>/dev/null):-?}')
+  # Using nvm_ls is much slower (also has vX.X.X instead of just X.X.X)
+  #__dko_prompt_right_colors+=('$( [ "${(e)$(nvm_ls current 2>/dev/null)}" = "$DKO_DEFAULT_NODE_VERSION" ] && echo "%F{blue}" || echo "%F{red}")')
+  __dko_prompt_right_colors+=('$( [[ "$__current_node" = "$DKO_DEFAULT_NODE_VERSION" ]] && echo "%F{blue}" || echo "%F{red}")')
+  __dko_prompt_right_parts+=($__current_node)
 }
 dko::has "pyenv" && {
   [[ "${#__dko_prompt_right_parts}" -ne 0 ]] && {
