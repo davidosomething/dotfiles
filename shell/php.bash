80de48618f26cf335c34e50d90d9ea0a926a747b
David O'Trakoun <me@davidosomething.com>
2017-07-31 22:48:56 -0400

    single export

--- shell/php.bash
+++ shell/php.bash
@@ -7,7 +7,7 @@ DKO_SOURCE="${DKO_SOURCE} -> shell/php.bash {"
 # ============================================================================
 
 dko::source "${DKO_BREW_PREFIX}/opt/php-version/php-version.sh" && {
-  export DKO_SOURCE="${DKO_SOURCE} -> php-version"
+  DKO_SOURCE="${DKO_SOURCE} -> php-version"
   php-version 7.1
 }
 
