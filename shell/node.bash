515254191fab62e3a30949540e093b41fb6b2a68
David O'Trakoun <dotrakoun@squarespace.com>
2017-08-09 11:31:40 -0400

    only need to determine default node once

--- shell/node.bash
+++ shell/node.bash
@@ -24,13 +24,15 @@ export NPM_CONFIG_SIGN_GIT_TAG="TRUE"
 export NVM_DIR="${XDG_CONFIG_HOME}/nvm"
 
 # using nvm? -- store default version for prompt compare
-dko::source "${NVM_DIR}/nvm.sh" && {
-  DKO_SOURCE="${DKO_SOURCE} -> nvm"
+dko::source "${NVM_DIR}/nvm.sh" \
+  && DKO_SOURCE="${DKO_SOURCE} -> nvm" \
+  && [ -z "$DKO_DEFAULT_NODE_VERSION" ] && {
   # Get initial nvm version using bash string manipulation instead of NVM
   # calls. While this is significantly faster, it is not correct if starting
   # a shell in a dir with a .nvmrc ~= default (which I almost never do).
   # Also does not use vX.X.X -- just X.X.X
-  DKO_DEFAULT_NODE_VERSION="${${NVM_BIN/$NVM_DIR\/versions\/node\/v}%\/b*}"
+  __nodir="${NVM_BIN/$NVM_DIR\/versions\/node\/v}"
+  DKO_DEFAULT_NODE_VERSION="${__nodir%\/bin}"
   #DKO_DEFAULT_NODE_VERSION="$(nvm version default)"
   export DKO_DEFAULT_NODE_VERSION
 }
