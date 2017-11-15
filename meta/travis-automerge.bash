7cd7b1f5f447d01576694431e9764059b772fe53
David O'Trakoun <me@davidosomething.com>
2017-11-15 00:43:16 -0500

    deploy script

--- meta/travis-automerge.bash
+++ meta/travis-automerge.bash
@@ -1,18 +1,12 @@
 #!/bin/bash -e
 
-# check for env vars
+echo "==> Checking env vars"
+: "${TRAVIS_COMMIT:?}"
+: "${TRAVIS_REPO_SLUG:?}"
 : "${encrypted_ea0d6abeb5c4_key:?}"
 : "${encrypted_ea0d6abeb5c4_iv:?}"
 
-export GIT_COMMITTER_EMAIL='travis-ci@davidosomething.com'
-export GIT_COMMITTER_NAME='Travis CI'
-
-# do full checkout
-repo_temp="$(mktemp -d)"
-git clone "https://github.com/${GITHUB_REPO}" "$repo_temp"
-cd "$repo_temp" || exit 1
-
-# setup deploy key
+echo "==> Setup deploy key"
 openssl aes-256-cbc \
   -K "$encrypted_ea0d6abeb5c4_key" \
   -iv "$encrypted_ea0d6abeb5c4_iv" \
@@ -22,7 +16,14 @@ eval "$(ssh-agent -s)"  # start the ssh agent
 chmod 600 local/deploy_key
 ssh-add local/deploy_key
 
-# merge and push to master
+echo "==> Cloning full repo"
+repo_temp="$(mktemp -d)"
+git clone "git@github.com:${TRAVIS_REPO_SLUG}.git" "$repo_temp"
+cd "$repo_temp" || exit 1
+
+echo "==> Merge into master and push"
+export GIT_COMMITTER_EMAIL='travis-ci@davidosomething.com'
+export GIT_COMMITTER_NAME='Travis CI'
 git checkout master
 git merge --ff-only "$TRAVIS_COMMIT"
-git push "git@github.com:${TRAVIS_REPO_SLUG}.git" master
+git push origin master
