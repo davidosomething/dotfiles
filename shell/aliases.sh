614b59af76952545606f0c86f38e74703f1179f6
David O'Trakoun <dotrakoun@squarespace.com>
2018-01-19 15:38:47 -0500

    add xcimg

--- shell/aliases.sh
+++ shell/aliases.sh
@@ -199,6 +199,7 @@ alias ta='tmux attach'
 
 alias cuios='XCODE_XCCONFIG_FILE="${PWD}/xcconfigs/swift31.xcconfig" carthage update --platform iOS'
 alias deletederived='rm -rf ~/Library/Developer/Xcode/DerivedData/*'
+alias xcimg="xcrun simctl addmedia booted"
 
 # ----------------------------------------------------------------------------
 # rest of bins
