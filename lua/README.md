d64377ae1c0bce38503a29877a35697d22c3aab3
David O'Trakoun <me@davidosomething.com>
2019-02-08 15:27:03 -0500

    lua setup

--- lua/README.md
+++
@@ -1,9 +0,0 @@
-Requires
-
-```sh
-luarocks install --local checks # formatter requires
-luarocks install --local formatter
-luarocks install --local ldoc
-luarocks install --local --server=http://luarocks.org/dev lua-lsp
-luarocks install --local luacheck
-```
--- shell/os-darwin.sh
+++ shell/os-darwin.sh
@@ -9,12 +9,18 @@ export DOTFILES_DISTRO="mac"
 # just assume brew is in normal location, don't even check for it
 export DKO_BREW_PREFIX="/usr/local"
 
+# ============================================================================
 # homebrew
+# ============================================================================
+
 export HOMEBREW_NO_ANALYTICS=1
 export HOMEBREW_NO_INSECURE_REDIRECT=1
 export HOMEBREW_CASK_OPTS='--require-sha'
 
+# ============================================================================
 # Allow pyenv to use custom openssl from brew
+# ============================================================================
+
 [ -d "${DKO_BREW_PREFIX}/opt/openssl/lib" ] &&
   export LDFLAGS="-L${DKO_BREW_PREFIX}/opt/openssl/lib"
 [ -d "${DKO_BREW_PREFIX}/opt/openssl/include" ] &&
@@ -22,6 +28,18 @@ export HOMEBREW_CASK_OPTS='--require-sha'
 
 [ -d "${DKO_BREW_PREFIX}/share/android-sdk" ] &&
   export ANDROID_SDK_ROOT="${DKO_BREW_PREFIX}/share/android-sdk"
+
+# ============================================================================
+# prefer homebrewed lua@5.1
+# ============================================================================
+
+command -v luarocks >/dev/null &&
+  [[ -d "${DKO_BREW_PREFIX}/opt/lua@5.1" ]] &&
+  {
+    export DKO_LUA_DIR="${DKO_BREW_PREFIX}/opt/lua@5.1"
+    eval "$(luarocks --lua-dir="$DKO_LUA_DIR" path --bin)"
+    alias luarocks='luarocks --lua-dir="$DKO_LUA_DIR"'
+  }
 
 # ============================================================================
 # Functions
