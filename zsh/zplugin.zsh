2d37c7208c597c9c0612caf723e294703007b955
David O'Trakoun <me@davidosomething.com>
2019-03-13 00:11:24 -0400

    quoting, forget up completions

--- zsh/zplugin.zsh
+++ zsh/zplugin.zsh
@@ -2,13 +2,13 @@
 
 DKO_SOURCE="${DKO_SOURCE} -> zplugin.zsh {"
 
-zplugin ice lucid wait'0' as"program" pick"git-ink"
+zplugin ice lucid wait'0' as'program' pick'git-ink'
 zplugin load davidosomething/git-ink
 
-zplugin ice lucid wait'0' as"program" pick"git-my"
+zplugin ice lucid wait'0' as'program' pick'git-my'
 zplugin load davidosomething/git-my
 
-zplugin ice lucid wait'0' as"program" pick"git-take"
+zplugin ice lucid wait'0' as'program' pick'git-take'
 zplugin load davidosomething/git-take
 
 # my fork of cdbk, ZSH hash based directory bookmarking
@@ -20,25 +20,29 @@ zplugin load 'davidosomething/cdbk'
 # Vendor: Commands
 # ----------------------------------------------------------------------------
 
-zplugin ice lucid wait'0' as"program" pick"${ZPFX}/bin/git-*" \
+zplugin ice lucid wait'0' as'program' pick"${ZPFX}/bin/git-*" \
   make"PREFIX=$ZPFX" nocompile
 zplugin load tj/git-extras
 # completions
 zplugin ice lucid wait'0'
 zplugin snippet "${ZPLGM[PLUGINS_DIR]}/tj---git-extras/etc/git-extras-completion.zsh"
 
-zplugin ice lucid wait'0' as"program" pick"git-open"
+zplugin ice lucid wait'0' as'program' pick'git-open'
 zplugin load paulirish/git-open
 
-zplugin ice lucid wait'0' as"program" pick"git-recent"
+zplugin ice lucid wait'0' as'program' pick'git-recent'
 zplugin load paulirish/git-recent
 
 # replaces up() in shell/functions.sh
 zplugin ice lucid wait'0'
 zplugin load 'shannonmoeller/up'
+zplugin cdclear -q # forget completions for up
 
 zplugin ice lucid wait'0'
 zplugin load 'wfxr/forgit'
+
+zplugin ice lucid wait'0' lucid as'program' pick'bin/git-dsf'
+zplugin light zdharma/zsh-diff-so-fancy
 
 # ----------------------------------------------------------------------------
 # Vendor: ZSH extension
@@ -55,7 +59,7 @@ export ZSH_AUTOSUGGEST_BUFFER_MAX_SIZE=48
 zplugin ice lucid wait'0' atload'_zsh_autosuggest_start'
 zplugin load 'zsh-users/zsh-autosuggestions'
 # clear the suggestion when entering completion select menu
-ZSH_AUTOSUGGEST_CLEAR_WIDGETS+=("expand-or-complete")
+ZSH_AUTOSUGGEST_CLEAR_WIDGETS+=('expand-or-complete')
 
 # ----------------------------------------------------------------------------
 # Vendor: Completion
@@ -64,7 +68,7 @@ ZSH_AUTOSUGGEST_CLEAR_WIDGETS+=("expand-or-complete")
 zplugin ice lucid wait'0'
 zplugin load 'zsh-users/zsh-completions'
 
-zplugin ice lucid wait'[[ -n ${ZLAST_COMMANDS[(r)grad*]} ]]' as"completion" nocompile
+zplugin ice lucid wait'[[ -n ${ZLAST_COMMANDS[(r)grad*]} ]]' as'completion' nocompile
 zplugin load 'gradle/gradle-completion'
 
 zplugin ice lucid wait'0'
@@ -84,7 +88,7 @@ zplugin load 'voronkovich/phpcs.plugin.zsh'
 # Syntax last, and compinit before it
 # ----------------------------------------------------------------------------
 
-zplugin ice lucid wait"0" atinit"zpcompinit; zpcdreplay"
+zplugin ice lucid wait'0' atinit'zpcompinit; zpcdreplay'
 zplugin load 'zdharma/fast-syntax-highlighting'
 
 export DKO_SOURCE="${DKO_SOURCE} }"
