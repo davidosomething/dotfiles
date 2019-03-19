cfa7ebd26cedaa4843cbdba4f87e206e9875bcd7
David O'Trakoun <dotrakoun@squarespace.com>
2019-03-19 16:13:31 -0400

    use bash to output box, remove boxen-cli

--- lib/pretty.bash
+++ lib/pretty.bash
@@ -3,14 +3,30 @@
 # std logging
 # Based on http://serverwizard.heroku.com/script/rvm+git
 # added error output to stderr
-__dko_echo()    { printf           '          %s\033[0;m\n' "$1"; }
-__dko_status()  { printf '\033[0;34m==>       %s\033[0;m\n' "$1"; }
+__dko_echo() { printf '          %s\033[0;m\n' "$1"; }
+__dko_status() { printf '\033[0;34m==>       %s\033[0;m\n' "$1"; }
 __dko_status_() { printf '\033[0;34m          %s\033[0;m\n' "$1"; }
-__dko_ok()      { printf '\033[0;32m==> OK:   %s\033[0;m\n' "$1"; }
-__dko_ok_()     { printf '\033[0;32m==>       %s\033[0;m\n' "$1"; }
-__dko_err()     { printf '\033[0;31m==> ERR:  %s\033[0;m\n' "$1" >&2; }
-__dko_err_()    { printf '\033[0;31m          %s\033[0;m\n' "$1" >&2; }
-__dko_warn()    { printf '\033[0;33m==> WARN: %s\033[0;m\n' "$1" >&2; }
-__dko_warn_()   { printf '\033[0;33m          %s\033[0;m\n' "$1" >&2; }
-__dko_usage()   { printf '\033[0;34m==> USAGE: \033[0;32m%s\033[0;m\n' "$1"; }
-__dko_usage_()  { printf '\033[0;29m           %s\033[0;m\n' "$1"; }
+__dko_ok() { printf '\033[0;32m==> OK:   %s\033[0;m\n' "$1"; }
+__dko_ok_() { printf '\033[0;32m==>       %s\033[0;m\n' "$1"; }
+__dko_err() { printf '\033[0;31m==> ERR:  %s\033[0;m\n' "$1" >&2; }
+__dko_err_() { printf '\033[0;31m          %s\033[0;m\n' "$1" >&2; }
+__dko_warn() { printf '\033[0;33m==> WARN: %s\033[0;m\n' "$1" >&2; }
+__dko_warn_() { printf '\033[0;33m          %s\033[0;m\n' "$1" >&2; }
+__dko_usage() { printf '\033[0;34m==> USAGE: \033[0;32m%s\033[0;m\n' "$1"; }
+__dko_usage_() { printf '\033[0;29m           %s\033[0;m\n' "$1"; }
+__dko_center() {
+  columns="$(tput cols)"
+  while IFS= read -r line; do
+    printf "%*s\n" $(((${#line} + columns) / 2)) "$line"
+  done <<<"$1"
+}
+__dko_line() {
+  columns="$(tput cols)"
+  printf %"$columns"s | tr " " "="
+  printf "\n"
+}
+__dko_header() {
+  __dko_line
+  __dko_center "$1"
+  __dko_line
+}
