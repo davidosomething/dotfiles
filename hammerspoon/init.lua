16245c6d17a4056643a177b7d8675dec91588ff4
David O'Trakoun <me@davidosomething.com>
2019-04-16 23:42:43 -0400

    show audio device in menu bar

--- hammerspoon/init.lua
+++ hammerspoon/init.lua
@@ -13,11 +13,32 @@ hs.hotkey.bind({"cmd", "ctrl"}, "V", function()
 end)
 
 ---
+-- Display audiosource
+-- setAudiosourceBarTitle(uid, event_name, event_scope, event_element)
+local audiosourceBar = hs.menubar.new()
+local function setAudiosourceBarTitle (e)
+  if e ~= 'dOut' then return end
+  local defaultDevice = hs.audiodevice.defaultOutputDevice()
+  local title = defaultDevice:name()
+  if title == 'Built-in Output' then title = 'H'
+  elseif title == 'Built-in Line Output' then title = 'L'
+  elseif title == 'Built-in Digital Output' then title = 'D'
+  end
+  audiosourceBar:setTitle(title)
+end
+setAudiosourceBarTitle('dOut')
+local audioWatcher = hs.audiodevice.watcher
+audioWatcher.setCallback(setAudiosourceBarTitle)
+audioWatcher.start()
+
+---
 -- clear seal cache and reload config
 hs.hotkey.bind(hyper, "R", function()
   spoon.Seal:refreshAllCommands()
+  if audioWatcher then audioWatcher.stop() end
+  if audiosourceBar then audiosourceBar:delete() end
+  hs.notify.show("Reloading Hammerspoon config", "Manually", "")
   hs.reload()
-  hs.notify.show("Hammerspoon config reloaded", "Manually", "")
 end)
 
 ---
