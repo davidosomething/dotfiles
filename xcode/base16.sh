d6abfed6569bb176069975f16bff8494392c8ddb
David O'Trakoun <dotrakoun@squarespace.com>
2019-05-08 11:40:15 -0400

    move mac apps stuff into mac/

--- xcode/base16.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-
-set -e
-
-mkdir -p ~/Library/Developer/Xcode/UserData/FontAndColorThemes
-cd -- ~/Library/Developer/Xcode/UserData/FontAndColorThemes || exit 1
-git clone https://github.com/joedynamite/base16-xcode Base16
-ln -s -- Base16/* ./
