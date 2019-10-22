18705b1fb8cc988e4d538425c41d19988fd513ee
David O'Trakoun <me@davidosomething.com>
2019-10-22 00:20:14 -0400

    ice no longer needed

--- zsh/zplugin.zsh
+++ zsh/zplugin.zsh
@@ -2,60 +2,60 @@
 
 DKO_SOURCE="${DKO_SOURCE} -> zplugin.zsh {"
 
-zplugin ice lucid wait as'program' pick'git-ink'
+zplugin lucid wait as'program' pick'git-ink'
 zplugin light davidosomething/git-ink
 
-zplugin ice lucid wait as'program' pick'git-my'
+zplugin lucid wait as'program' pick'git-my'
 zplugin light davidosomething/git-my
 
-zplugin ice lucid wait as'program' pick'git-take'
+zplugin lucid wait as'program' pick'git-take'
 zplugin light davidosomething/git-take
 
 # my fork of cdbk, ZSH hash based directory bookmarking. No wait.
 export ZSH_BOOKMARKS="${HOME}/.local/zshbookmarks"
-zplugin ice lucid
+zplugin lucid
 zplugin light 'davidosomething/cdbk'
 
 # ----------------------------------------------------------------------------
 # Vendor: Commands
 # ----------------------------------------------------------------------------
 
-zplugin ice lucid as'program' \
+zplugin lucid as'program' \
   pick"${ZPFX}/bin/git-*" \
   src"etc/git-extras-completion.zsh" \
   make"PREFIX=$ZPFX"
 zplugin light tj/git-extras
 
 # `` compl for git commands
-zplugin ice lucid wait
+zplugin lucid wait
 zplugin light 'hschne/fzf-git'
 
-zplugin ice lucid wait as'program' pick'git-open'
+zplugin lucid wait as'program' pick'git-open'
 zplugin light paulirish/git-open
 
-zplugin ice lucid wait as'program' pick'git-recent'
+zplugin lucid wait as'program' pick'git-recent'
 zplugin light paulirish/git-recent
 
 # replaces up() in shell/functions.sh. No wait.
-zplugin ice lucid nocompletions
+zplugin lucid nocompletions
 zplugin light 'shannonmoeller/up'
 
-zplugin ice lucid nocompletions
+zplugin lucid nocompletions
 zplugin light 'skywind3000/z.lua'
 
 # gi is my git-ink alias, and i don't need a .gitignore generator
 export forgit_ignore='fgi'
-zplugin ice lucid wait
+zplugin lucid wait
 zplugin light 'wfxr/forgit'
 
-zplugin ice lucid wait lucid as'program' pick'bin/git-dsf'
+zplugin lucid wait lucid as'program' pick'bin/git-dsf'
 zplugin light zdharma/zsh-diff-so-fancy
 
 # ----------------------------------------------------------------------------
 # Vendor: ZSH extension
 # ----------------------------------------------------------------------------
 
-zplugin ice lucid wait'[[ -n ${ZLAST_COMMANDS[(r)man*]} ]]'
+zplugin lucid wait'[[ -n ${ZLAST_COMMANDS[(r)man*]} ]]'
 zplugin snippet OMZ::plugins/colored-man-pages/colored-man-pages.plugin.zsh
 
 # In-line best history match suggestion
@@ -63,7 +63,7 @@ zplugin snippet OMZ::plugins/colored-man-pages/colored-man-pages.plugin.zsh
 export ZSH_AUTOSUGGEST_BUFFER_MAX_SIZE=48
 # as of v4.0 use ZSH/zpty module to async retrieve
 #export ZSH_AUTOSUGGEST_USE_ASYNC=1
-zplugin ice lucid wait atload'_zsh_autosuggest_start'
+zplugin lucid wait atload'_zsh_autosuggest_start'
 zplugin load 'zsh-users/zsh-autosuggestions'
 # clear the suggestion when entering completion select menu
 ZSH_AUTOSUGGEST_CLEAR_WIDGETS+=('expand-or-complete')
@@ -72,16 +72,16 @@ ZSH_AUTOSUGGEST_CLEAR_WIDGETS+=('expand-or-complete')
 # Vendor: Completion
 # ----------------------------------------------------------------------------
 
-zplugin ice blockf lucid wait
+zplugin blockf lucid wait
 zplugin load 'zsh-users/zsh-completions'
 
-zplugin ice lucid wait
+zplugin lucid wait
 zplugin light 'voronkovich/phpcs.plugin.zsh'
 
 
 [[ -f "${TRAVIS_CONFIG_PATH}/travis.sh" ]] \
   && {
-    zplugin ice lucid wait
+    zplugin lucid wait
     zplugin light "$TRAVIS_CONFIG_PATH"
   }
 
@@ -89,7 +89,7 @@ zplugin light 'voronkovich/phpcs.plugin.zsh'
 # Syntax last, and compinit before it
 # ----------------------------------------------------------------------------
 
-zplugin ice lucid wait'1' atinit'zpcompinit; zpcdreplay'
+zplugin lucid wait'1' atinit'zpcompinit; zpcdreplay'
 zplugin load 'zdharma/fast-syntax-highlighting'
 
 export DKO_SOURCE="${DKO_SOURCE} }"
