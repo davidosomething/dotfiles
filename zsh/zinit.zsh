daa2926b5f8c0885e9855e368b3a671729edca3a
David O'Trakoun <dotrakoun@squarespace.com>
2020-04-10 15:46:52 -0400

    i swear to god @psprint

--- zsh/zinit.zsh
+++ zsh/zinit.zsh
@@ -17,20 +17,19 @@ __dko_has docker && {
 # Git
 # ----------------------------------------------------------------------------
 
-zinit for light-mode lucid wait as'program' pick davidosomething/git-ink
-zinit for light-mode lucid wait as'program' pick davidosomething/git-my
-zinit for light-mode lucid wait as'program' pick davidosomething/git-take
-zinit for light-mode lucid wait as'program' pick paulirish/git-open
-zinit for light-mode lucid wait as'program' pick paulirish/git-recent
+zinit lucid wait as'program' pick for \
+  light-mode davidosomething/git-ink \
+  light-mode davidosomething/git-my \
+  light-mode davidosomething/git-take \
+  light-mode paulirish/git-open \
+  light-mode paulirish/git-recent \
+  pick'bin/git-dsf' light-mode 'zdharma/zsh-diff-so-fancy'
 
-zinit for lucid wait as'program' \
+zinit lucid wait as'program' \
   pick"${ZPFX}/bin/git-*" \
   src'etc/git-extras-completion.zsh' \
   make"PREFIX=${ZPFX}" \
-  light-mode 'tj/git-extras'
-
-zinit for lucid wait as'program' pick'bin/git-dsf' \
-  light-mode 'zdharma/zsh-diff-so-fancy'
+  for light-mode 'tj/git-extras'
 
 # ----------------------------------------------------------------------------
 # FZF + Git
@@ -39,15 +38,15 @@ zinit for lucid wait as'program' pick'bin/git-dsf' \
 ! __dko_has fzf && {
   # Binary release in archive, from GitHub-releases page.
   # After automatic unpacking it provides program "fzf".
-  zinit for lucid wait from'gh-r' as'program' light-mode 'junegunn/fzf-bin'
+  zinit lucid wait from'gh-r' as'program' for light-mode 'junegunn/fzf-bin'
 }
 
 # gi is my git-ink alias, and i don't need a .gitignore generator
 export forgit_ignore='fgi'
-zinit for lucid wait light-mode 'wfxr/forgit'
+zinit lucid wait for light-mode 'wfxr/forgit'
 
 # `` compl for git commands
-zinit for lucid wait light-mode 'hschne/fzf-git'
+zinit lucid wait for light-mode 'hschne/fzf-git'
 
 # ----------------------------------------------------------------------------
 # Misc
@@ -55,26 +54,18 @@ zinit for lucid wait light-mode 'hschne/fzf-git'
 
 # my fork of cdbk, ZSH hash based directory bookmarking. No wait!
 export ZSH_BOOKMARKS="${HOME}/.local/zshbookmarks"
-zinit for lucid light-mode 'davidosomething/cdbk'
+zinit lucid for light-mode 'davidosomething/cdbk'
 
-zinit for lucid wait from'gh-r' as'program' \
-  mv'bat* -> bat' pick'bat/bat' \
-  light-mode '@sharkdp/bat'
-zinit for lucid wait from'gh-r' as'program' \
-  pick'delta*/delta' \
-  atload'GIT_PAGER="delta --dark"' \
-  light-mode '@dandavison/delta'
-zinit for lucid wait from'gh-r' as'program' \
-  mv'fd* -> fd' pick'fd/fd' \
-  light-mode '@sharkdp/fd'
-zinit for lucid wait from'gh-r' as'program' \
-  mv'shfmt* -> shfmt' pick'mvdan/sh' \
-  light-mode '@mvdan/sh'
+zinit lucid wait from'gh-r' as'program' for \
+  mv'bat* -> bat' pick'bat/bat' light-mode '@sharkdp/bat' \
+  pick'delta*/delta' atload'GIT_PAGER="delta --dark"' light-mode '@dandavison/delta' \
+  mv'fd* -> fd' pick'fd/fd' light-mode '@sharkdp/fd' \
+  mv'shfmt* -> shfmt' pick'mvdan/sh' light-mode '@mvdan/sh'
 
 __dko_has cargo && {
   export _ZO_DATA="${XDG_DATA_HOME}/zoxide"
-  zinit for lucid atclone'cargo install zoxide' atpull'%atclone' \
-    light-mode 'ajeetdsouza/zoxide'
+  zinit lucid atclone'cargo install zoxide' atpull'%atclone' \
+    for light-mode 'ajeetdsouza/zoxide'
   alias j=z
 }
 
@@ -83,7 +74,7 @@ __dko_has z || {
   export _ZL_DATA="${XDG_DATA_HOME}/zlua"
   export _ZL_HYPHEN=1
   export _ZL_NO_ALIASES=1
-  zinit for lucid nocompletions light-mode 'skywind3000/z.lua'
+  zinit lucid nocompletions for light-mode 'skywind3000/z.lua'
 
   # redefine up to work like shannonmoeller/up
   up() {
@@ -103,14 +94,14 @@ __dko_has z || {
 }
 
 __dko_has up || {
-  zinit for lucid nocompletions light-mode '@shannonmoeller/up'
+  zinit lucid nocompletions for light-mode '@shannonmoeller/up'
 }
 
 # ----------------------------------------------------------------------------
 # ZSH extensions
 # ----------------------------------------------------------------------------
 
-zinit for lucid trigger-load'!man' is-snippet \
+zinit lucid trigger-load'!man' for is-snippet \
   'OMZ::plugins/colored-man-pages/colored-man-pages.plugin.zsh'
 
 # In-line best history match suggestion
@@ -125,20 +116,20 @@ export ZSH_AUTOSUGGEST_ACCEPT_WIDGETS=(
 )
 
 # ! will track the loading since using zinit load
-zinit for lucid wait \
+zinit lucid wait \
   atload'_zsh_autosuggest_start && bindkey "^k" autosuggest-accept' \
-  'zsh-users/zsh-autosuggestions'
+  for 'zsh-users/zsh-autosuggestions'
 # clear the suggestion when entering completion select menu
 
 # ----------------------------------------------------------------------------
 # Vendor: Completion
 # ----------------------------------------------------------------------------
 
-zinit for lucid wait blockf atpull'zinit creinstall -q .' \
-  'zsh-users/zsh-completions'
+zinit lucid wait blockf atpull'zinit creinstall -q .' \
+  for 'zsh-users/zsh-completions'
 
 [[ -f "${TRAVIS_CONFIG_PATH}/travis.sh" ]] && {
-  zinit for lucid wait light-mode "$TRAVIS_CONFIG_PATH"
+  zinit lucid wait for light-mode "$TRAVIS_CONFIG_PATH"
 }
 
 # ----------------------------------------------------------------------------
@@ -146,7 +137,7 @@ zinit for lucid wait blockf atpull'zinit creinstall -q .' \
 # ----------------------------------------------------------------------------
 
 # clear the suggestion when entering completion select menu
-zinit for lucid wait atinit"ZINIT[COMPINIT_OPTS]=-C; zicompinit; zicdreplay" \
-  'zdharma/fast-syntax-highlighting'
+zinit lucid wait atinit"ZINIT[COMPINIT_OPTS]=-C; zicompinit; zicdreplay" \
+  for 'zdharma/fast-syntax-highlighting'
 
 DKO_SOURCE="${DKO_SOURCE} }"
