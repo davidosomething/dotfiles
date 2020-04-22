7711b17e0fa48f07dab1b856e73bfc92acc9a4d5
David O'Trakoun <me@davidosomething.com>
2020-04-21 22:19:15 -0400

    hammerspoon throw window to other monitor

--- mac/hammerspoon/window.lua
+++ mac/hammerspoon/window.lua
@@ -40,3 +40,23 @@ spoon.Lunette:bindHotkeys({
   nextDisplay = false,
   prevDisplay = false,
 })
+
+hs.hotkey.bind(hyper, ']', function()
+  -- get the focused window
+  local win = hs.window.focusedWindow()
+  -- get the screen where the focused window is displayed, a.k.a. current screen
+  local screen = win:screen()
+  -- compute the unitRect of the focused window relative to the current screen
+  -- and move the window to the next screen setting the same unitRect 
+  win:move(win:frame():toUnitRect(screen:frame()), screen:next(), true, 0)
+end)
+
+hs.hotkey.bind(hyper, '[', function()
+  -- get the focused window
+  local win = hs.window.focusedWindow()
+  -- get the screen where the focused window is displayed, a.k.a. current screen
+  local screen = win:screen()
+  -- compute the unitRect of the focused window relative to the current screen
+  -- and move the window to the next screen setting the same unitRect 
+  win:move(win:frame():toUnitRect(screen:frame()), screen:previous(), true, 0)
+end)
