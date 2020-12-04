6b925d338982af18c1a263c669f2bfc423d21635
David O'Trakoun <me@davidosomething.com>
2020-12-03 22:17:31 -0500

    app shortcuts

--- mac/hammerspoon/launcher.lua
+++ mac/hammerspoon/launcher.lua
@@ -8,3 +8,31 @@ spoon.Seal:bindHotkeys({
   show = {{"cmd"}, "Space"}
 })
 spoon.Seal:start()
+
+hs.hotkey.bind(hyper, 'a', function()
+  hs.application.launchOrFocus("Authy Desktop")
+end)
+
+hs.hotkey.bind(hyper, 'b', function()
+  hs.application.launchOrFocus("Bitwarden")
+end)
+
+hs.hotkey.bind(hyper, 'c', function()
+  hs.application.launchOrFocus("Google Chrome")
+end)
+
+hs.hotkey.bind(hyper, 'd', function()
+  hs.application.launchOrFocus("Discord")
+end)
+
+hs.hotkey.bind(hyper, 'i', function()
+  hs.application.launchOrFocus("iTerm")
+end)
+
+hs.hotkey.bind(hyper, 'n', function()
+  hs.application.launchOrFocus("Joplin")
+end)
+
+hs.hotkey.bind(hyper, 's', function()
+  hs.application.launchOrFocus("Slack")
+end)
