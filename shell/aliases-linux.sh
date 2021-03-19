469724f1f89f1f02b651faebd1c46a6e50ab7266
David O'Trakoun <me@davidosomething.com>
2021-03-18 23:28:05 -0400

    use dko-open on linux

--- shell/aliases-linux.sh
+++ shell/aliases-linux.sh
@@ -2,6 +2,8 @@
 
 export DKO_SOURCE="${DKO_SOURCE} -> shell/aliases-linux.sh"
 
+alias open="dko-open"
+
 alias startx='startx > "${DOTFILES}/logs/startx.log" 2>&1'
 
 alias testnotification="notify-send \
