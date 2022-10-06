40460f24e35bf7f0f6b67a03581a44f815d4e015
David O'Trakoun <dotrakoun@clearstreet.io>
2022-10-06 15:34:42 -0400

    tmux 2.9+ conf

--- tmux/versions.bash
+++ tmux/versions.bash
@@ -2,11 +2,14 @@
 
 __load() {
   readonly tmux_version
-  tmux_version="$(tmux -V | cut -c 6-)"
+  # remove non-digits, so tmux 3.0a becomes 3.0
+  tmux_version="$(tmux -V | sed 's/[^0-9.]*//g')"
 
-  if (( $(echo "$tmux_version >= 2.4" | bc) == 1 )); then
+  if (( $(echo "$tmux_version >= 2.9" | bc -l) )); then
+    tmux source-file "${DOTFILES}/tmux/t2.9.conf"
+  elif (( $(echo "$tmux_version >= 2.4" | bc -l) )); then
     tmux source-file "${DOTFILES}/tmux/t2.4.conf"
-  elif (( $(echo "$tmux_version >= 2.1" | bc) == 1 )); then
+  elif (( $(echo "$$tmux_version >= 2.1" | bc -l) )); then
     tmux source-file "${DOTFILES}/tmux/t2.conf"
   else
     tmux source-file "${DOTFILES}/tmux/t1.conf"
