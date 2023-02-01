3972e389acb75bcfa2a83289831262a6c83e6bac
David O'Trakoun <me@davidosomething.com>
2023-01-31 23:43:00 -0500

    move more settings to lua

--- nvim/init.vim
+++ nvim/init.vim
@@ -23,170 +23,16 @@ let g:truecolor = has('termguicolors')
       \ && ($COLORTERM ==# 'truecolor' || $DOTFILES_OS ==# 'Darwin')
 
 " ============================================================================
-" Temporary fixes
-" ============================================================================
-
-if !has('nvim-0.4')
-  " https://github.com/neovim/neovim/issues/7994
-  augroup dkoneovimfixes
-    autocmd!
-    autocmd InsertLeave * set nopaste
-  augroup END
-endif
-
-" The default blinking cursor leaves random artifacts in display like "q" in
-" old terminal emulators and some VTEs
-" https://github.com/neovim/neovim/issues?utf8=%E2%9C%93&q=is%3Aissue+cursor+shape+q
-set guicursor=
-augroup dkonvim
-  autocmd!
-  autocmd OptionSet guicursor noautocmd set guicursor=
-augroup END
-
-" ============================================================================
 " Settings
 " ============================================================================
 
 lua require('dko.opt')
-
-" ----------------------------------------------------------------------------
-" Window splitting and buffers
-" ----------------------------------------------------------------------------
-
-set splitbelow
-set splitright
-if exists('+splitkeep')
-  set splitkeep=screen
-endif
-
-set hidden                            " remember undo after quitting
-
-" reveal already opened files from the quickfix window instead of opening new
-" buffers
-set switchbuf=useopen
-
-set nostartofline                     " don't jump to col1 on switch buffer
-
-" ----------------------------------------------------------------------------
-" Code folding
-" ----------------------------------------------------------------------------
-
-set foldlevel=999                     " very high === all folds open
-set foldlevelstart=99                 " show all folds by default
-set nofoldenable
-
-" ----------------------------------------------------------------------------
-" Trailing whitespace
-" ----------------------------------------------------------------------------
-
-set list
-set listchars=
-set listchars+=tab:→\ 
-set listchars+=trail:·
-set listchars+=extends:»              " show cut off when nowrap
-set listchars+=precedes:«
-set listchars+=nbsp:⣿
-
-" maybe...
-" if has('patch-7.4.785') | set fixendofline | endif
-
-" ----------------------------------------------------------------------------
-" Diffing
-" ----------------------------------------------------------------------------
-
-" Note this is += since fillchars was defined in the window config
-set fillchars+=diff:⣿
-set diffopt=vertical                  " Use in vertical diff mode
-set diffopt+=filler                   " blank lines to keep sides aligned
-set diffopt+=iwhite                   " Ignore whitespace changes
-
-" ----------------------------------------------------------------------------
-" Input auto-formatting (global defaults)
-" Probably need to update these in after/ftplugin too since ftplugins will
-" probably update it.
-" ----------------------------------------------------------------------------
-
-set formatoptions=
-set formatoptions+=r                  " Continue comments by default
-set formatoptions-=o                  " do not continue comment using o or O
-set formatoptions-=a                  " auto-gq on type in comments?
-set formatoptions+=n                  " Recognize numbered lists
-set formatoptions+=2                  " Use indent from 2nd line of a paragraph
-set formatoptions-=l                  " break lines that are already long?
-set formatoptions+=1                  " Break before 1-letter words
-" no // comment when joining commented lines. This is not a default neovim
-" setting despite what :help says. Various filetypes override it
-if has('patch-7.3.541') | set formatoptions+=j | endif
-
-" ----------------------------------------------------------------------------
-" Whitespace
-" ----------------------------------------------------------------------------
-
-set nowrap
-set nojoinspaces                      " J command doesn't add extra space
-
-" ----------------------------------------------------------------------------
-" Indenting - overridden by indent plugins
-" ----------------------------------------------------------------------------
-
-" For autoindent, use same spaces/tabs mix as previous line, even if
-" tabs/spaces are mixed. Helps for docblock, where the block comments have a
-" space after the indent to align asterisks
-"
-" The test case what happens when using o/O and >> and << on these:
-"
-"     /**
-"      *
-"
-" Refer also to formatoptions+=o (copy comment indent to newline)
-set nocopyindent
-
-" Try not to change the indent structure on "<<" and ">>" commands. I.e. keep
-" block comments aligned with space if there is a space there.
-set nopreserveindent
-
-" Smart detect when in braces and parens. Has annoying side effect that it
-" won't indent lines beginning with '#'. Relying on syntax indentexpr instead.
-" 'smartindent' in general is a piece of garbage, never turn it on.
-set nosmartindent
-
-" Global setting. I don't edit C-style code all the time so don't default to
-" C-style indenting.
-set nocindent
-
-" ----------------------------------------------------------------------------
-" Tabbing - overridden by editorconfig, after/ftplugin
-" ----------------------------------------------------------------------------
-
-" use multiple of shiftwidth when shifting indent levels.
-" this is OFF so block comments don't get fudged when using ">>" and "<<"
-set noshiftround
-
-" ----------------------------------------------------------------------------
-" Match and search
-" ----------------------------------------------------------------------------
-
-set matchtime=1                       " tenths of a sec
-set noshowmatch                       " briefly jump to matching paren?
-set wrapscan                          " Searches wrap around end of the file.
-set ignorecase
-" Follow smartcase and ignorecase when doing tag search
-if exists('+tagcase') && has('patch-7.4.2230') | set tagcase=followscs | endif
-set smartcase
 
 if !empty(dko#grepper#Get().command)
   let &g:grepprg = dko#grepper#Get().command . ' '
         \ . join(dko#grepper#Get().options, ' ')
   let &g:grepformat = dko#grepper#Get().format
 endif
-
-" ============================================================================
-" Security
-" ============================================================================
-
-" Disallow unsafe local vimrc commands
-" Leave down here since it trims local settings
-set secure
 
 " ============================================================================
 " :terminal emulator
@@ -480,5 +326,13 @@ augroup dkorestoreposition
   autocmd!
   autocmd BufWinEnter * call dko#RestorePosition()
 augroup END
+
+" ============================================================================
+" Security
+" ============================================================================
+
+" Disallow unsafe local vimrc commands
+" Leave down here since it trims local settings
+set secure
 
 " vim: sw=2 :
--- nvim/lua/dko/opt.lua
+++ nvim/lua/dko/opt.lua
@@ -1,78 +1,33 @@
-vim.opt.clipboard = 'unnamedplus'
+-- ===========================================================================
+-- Vim command engines
+-- ---------------------------------------------------------------------------
 
-vim.opt.timeout = false -- don't wait
+if vim.fn.exists('+pyxversion') == 1 and vim.fn.has('python3') == 1 then
+  vim.opt.pyxversion = 3
+end
+
+-- ===========================================================================
+-- File reading
+-- ===========================================================================
+
+-- Prior versions are super dangerous
+if vim.fn.has('patch-8.1.1365') == 0 and vim.fn.has('nvim-0.3.6') == 0 then
+  vim.opt.modeline = false
+else
+  -- Only check one line
+  if vim.fn.exists('+modelines') == 1 then
+    vim.opt.modelines = 1
+  end
+end
+
+-- ===========================================================================
+-- File saving
+-- ===========================================================================
 
 -- Bumped '100 to '1000 to save more previous files
 -- Bumped <50 to <100 to save more register lines
 -- Bumped s10 to s100 for to allow up to 100kb of data per item
 vim.opt.shada = "!,'1000,<100,s100,h"
-
--- New neovim feature, it's like vim-over but hides the thing being replaced
--- so it is not practical for now (makes it harder to remember what you're
--- replacing/reference previous regex tokens). Default is off, but explicitly
--- disabled here, too.
--- https://github.com/neovim/neovim/pull/5226
-vim.opt.inccommand = ''
-
--- Swap write and CursorHold timeout
--- Pretty quick... errorprone on old vim so only apply to nvim
-vim.opt.updatetime = 250
-
--- Prior versions are super dangerous
-if not vim.fn.has('patch-8.1.1365') and not vim.fn.has('nvim-0.3.6') then
-  vim.opt.modeline = false
-else
-  -- Only check one line
-  if vim.fn.exists('+modelines') then
-    vim.opt.modelines = 1
-  end
-end
-
-if vim.fn.exists('+pyxversion') and vim.fn.has('python3') then
-  vim.opt.pyxversion = 3
-end
-
--- ===========================================================================
--- Display
--- ===========================================================================
-
-vim.opt.title = true -- wintitle = filename - vim
-
-vim.opt.number = true
-vim.opt.numberwidth = 5
-
--- show context around current cursor position
-vim.opt.scrolloff = 8
-vim.opt.sidescrolloff = 16
-
-vim.opt.textwidth = 78
--- the line will be right after column 80, &tw+3
-vim.opt.colorcolumn = { '+3', '120' }
-vim.opt.cursorline = true
-
-vim.opt.synmaxcol = 512 -- don't syntax highlight long lines
-
-if vim.fn.exists('+signcolumn') then
-  vim.opt.signcolumn = vim.fn.has('nvim-0.4') and 'auto:3' or 'yes'
-end
-
-vim.opt.showtabline = 0 -- start OFF, toggle =2 to show tabline
-
--- This is slow on some terminals and often gets hidden by msgs so leave it off
-vim.opt.showcmd = false
-vim.opt.showmode = false -- don't show -- INSERT -- in cmdline
-
--- ===========================================================================
--- Wild and file globbing stuff in command mode
--- ===========================================================================
-
-vim.opt.browsedir = 'buffer' -- browse files in same dir as open file
-vim.opt.wildmode = 'list:longest,full'
-vim.opt.wildignorecase = true
-
--- ===========================================================================
--- File saving
--- ===========================================================================
 
 vim.opt.fileformats = 'unix,mac,dos'
 
@@ -94,6 +49,58 @@ vim.opt.backupcopy = 'yes'
 
 vim.opt.backupskip:append('/private/tmp/*') -- needed to edit crontab files
 vim.opt.backupskip:append('~/.secret/*')
+vim.opt.clipboard = 'unnamedplus'
+
+-- ===========================================================================
+-- Display
+-- ===========================================================================
+
+vim.opt.timeout = false -- don't wait
+
+-- Swap write and CursorHold timeout
+-- Pretty quick... errorprone on old vim so only apply to nvim
+vim.opt.updatetime = 250
+
+vim.opt.title = true -- wintitle = filename - vim
+
+vim.opt.number = true
+vim.opt.numberwidth = 5
+
+-- show context around current cursor position
+vim.opt.scrolloff = 8
+vim.opt.sidescrolloff = 16
+
+if vim.fn.exists('+signcolumn') == 1 then
+  vim.opt.signcolumn = vim.fn.has('nvim-0.4') and 'auto:3' or 'yes'
+end
+
+vim.opt.synmaxcol = 512 -- don't syntax highlight long lines
+
+vim.opt.textwidth = 78
+
+-- the line will be right after column 80, &tw+3
+vim.opt.colorcolumn = { '+3', '120' }
+vim.opt.cursorline = true
+
+vim.opt.showtabline = 0 -- start OFF, toggle =2 to show tabline
+
+-- This is slow on some terminals and often gets hidden by msgs so leave it off
+vim.opt.showcmd = false
+vim.opt.showmode = false -- don't show -- INSERT -- in cmdline
+
+-- like vim-over but hides the thing being replaced so it is not practical for
+-- now (makes it harder to remember what you're replacing/reference previous
+-- regex tokens).
+-- https://github.com/neovim/neovim/pull/5226
+vim.opt.inccommand = ''
+
+-- ===========================================================================
+-- Wild and file globbing stuff in command mode
+-- ===========================================================================
+
+vim.opt.browsedir = 'buffer' -- browse files in same dir as open file
+vim.opt.wildmode = 'list:longest,full'
+vim.opt.wildignorecase = true
 
 -- ===========================================================================
 -- Built-in completion
@@ -118,6 +125,123 @@ vim.opt.shortmess:append({
   c = true, -- Disable "Pattern not found" messages
   m = true, -- use "[+]" instead of "[Modified]"
   r = true, -- use "[RO]" instead of "[readonly]"
-	I = true, -- don't give the intro message when starting Vim |:intro|.
+  I = true, -- don't give the intro message when starting Vim |:intro|.
   W = true, -- don't give "written" or "[w]" when writing a file
 })
+
+-- ===========================================================================
+-- Window splitting and buffers
+-- ===========================================================================
+
+vim.opt.splitbelow = true
+vim.opt.splitright = true
+if vim.fn.exists('+splitkeep') == 1 then
+  vim.opt.splitkeep = 'screen'
+end
+
+vim.opt.hidden = true -- remember undo after quitting
+
+-- reveal already opened files from the quickfix window instead of opening new
+-- buffers
+vim.opt.switchbuf = 'useopen'
+
+-- ===========================================================================
+-- Code folding
+-- ===========================================================================
+
+vim.opt.foldlevel = 999 -- very high === all folds open
+vim.opt.foldlevelstart = 99 -- show all folds by default
+vim.opt.foldenable = false
+
+-- ===========================================================================
+-- Trailing whitespace
+-- ===========================================================================
+
+vim.opt.list = true
+vim.opt.listchars = {
+  extends = '»',
+  nbsp = '⣿',
+  precedes = '«',
+  trail = '·',
+  tab = '▶ ', -- this must be two chars, see :h listchars
+}
+
+-- ===========================================================================
+-- Diffing
+-- ===========================================================================
+
+vim.opt.fillchars:append({ diff = '⣿' })
+vim.opt.diffopt = {
+  vertical = true, -- Use in vertical diff mode
+  filler = true, -- blank lines to keep sides aligned
+  iwhite = true,  -- Ignore whitespace changes
+}
+
+-- ===========================================================================
+-- Input auto-formatting (global defaults)
+-- Probably need to update these in after/ftplugin too since ftplugins will
+-- probably update it.
+-- ===========================================================================
+
+vim.opt.formatoptions = {
+  j = true, -- remove comment leader on join comments
+  n = true, -- Recognize numbered lists
+  q = true, -- allow gq to format comments
+  o = true, -- continue comment using o or O
+  r = true, -- Continue comments by default
+  a = false, -- auto-gq paragraphs
+  l = false, -- break lines that are already long?
+}
+vim.opt.formatoptions:append('1') -- Break before 1-letter words
+vim.opt.formatoptions:append('2') -- Use indent from 2nd line of a paragraph
+
+vim.opt.wrap = false
+vim.opt.joinspaces = false -- J command doesn't add extra space
+
+-- ===========================================================================
+-- Indenting - overridden by indent plugins
+-- ===========================================================================
+
+-- For autoindent, use same spaces/tabs mix as previous line, even if
+-- tabs/spaces are mixed. Helps for docblock, where the block comments have a
+-- space after the indent to align asterisks
+--
+-- The test case what happens when using o/O and >> and << on these:
+--     /**
+--      *
+-- Refer also to formatoptions+=o (copy comment indent to newline)
+vim.opt.copyindent = false
+
+-- Try not to change the indent structure on "<<" and ">>" commands. I.e. keep
+-- block comments aligned with space if there is a space there.
+vim.opt.preserveindent = false
+
+-- Smart detect when in braces and parens. Has annoying side effect that it
+-- won't indent lines beginning with '#'. Relying on syntax indentexpr instead.
+-- 'smartindent' in general is a piece of garbage, never turn it on.
+vim.opt.smartindent = false
+
+-- Global setting. I don't edit C-style code all the time so don't default to
+-- C-style indenting.
+vim.opt.cindent = false
+
+-- ===========================================================================
+-- Tabbing - overridden by editorconfig, after/ftplugin
+-- ===========================================================================
+
+-- use multiple of shiftwidth when shifting indent levels.
+-- this is OFF so block comments don't get fudged when using ">>" and "<<"
+vim.opt.shiftround = false
+
+-- ===========================================================================
+-- Match and search
+-- ===========================================================================
+
+vim.opt.matchtime = 1 -- tenths of a sec
+vim.opt.showmatch = false -- briefly jump to matching paren?
+vim.opt.wrapscan = true -- Searches wrap around end of the file.
+vim.opt.ignorecase = true
+vim.opt.smartcase = true
+-- Follow smartcase and ignorecase when doing tag search
+vim.opt.tagcase = 'followscs'
+
