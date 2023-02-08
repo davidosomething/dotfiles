ff19bf674fccad7cf971e214bb1805b49a950373
David O'Trakoun <me@davidosomething.com>
2023-02-08 10:40:38 -0500

    tweak help matching'

--- nvim/lua/dko/help.lua
+++ nvim/lua/dko/help.lua
@@ -1,32 +1,30 @@
 return function()
   local cexpr = vim.fn.expand('<cexpr>')
-  local fn
+  local match
   if string.find(cexpr, "vim%.g%.") then
     local prefixed = "g:" .. cexpr:gsub("vim%.g%.(.-)$", "%1")
     print('Looking up vim.g ' .. prefixed)
     vim.cmd.help(prefixed)
     return
   elseif string.find(cexpr, "vim%.o%.") then
-    local quoted = "'" .. cexpr:gsub("vim%.o%.(.-)$", "%1") .. "'"
-    print('Looking up vim.o ' .. quoted)
-    vim.cmd.help(quoted)
-    return
+    match = "'" .. cexpr:gsub("vim%.o%.(.-)$", "%1") .. "'"
   elseif string.find(cexpr, "vim%.opt%.") then
-    local quoted = "'" .. cexpr:gsub("vim%.opt%.(.-)$", "%1"):gsub("(.*):.*$", "%1") .. "'"
-    print('Looking up vim.opt ' .. quoted)
-    vim.cmd.help(quoted)
-    return
-  elseif string.find(cexpr, "vim.fn.") then
-    fn = cexpr:gsub("vim%.fn%.(.-)%(.*$", "%1")
-  elseif string.find(cexpr, "vim.cmd.") then
-    fn = cexpr:gsub("vim%.cmd%.(.-)%(.*$", "%1")
-  elseif string.find(cexpr, "vim%.[api|lsp]") then
-    fn = cexpr:gsub("(vim%..-)%(.*$", "%1")
+    match = "'" .. cexpr:gsub("vim%.opt%.(.-)$", "%1"):gsub("(.*):.*$", "%1") .. "'"
+  elseif string.find(cexpr, "vim%.[cmd|fn]") then
+    -- vim.xyz.this(abc) -> this
+    match = cexpr:gsub("vim%..-%.(.*)", "%1")
+    match = match:gsub("(.*)%(.*", "%1")
+  elseif string.find(cexpr, "vim%.[api|diagnostic|keymap|lsp]") then
+    -- vim.xyz.this(abc) -> vim.xyz.this
+    match = cexpr:gsub("(vim%..-)%(.*$", "%1")
+  elseif string.find(cexpr, "string%.") then
+    -- string.xyz(abc) -> string.xyz
+    match = cexpr:gsub("(%a%..-)%(.*$", "%1")
   else
-    fn = 'luaref-' .. cexpr:gsub("(.*)%(.*$", "%1")
+    match = 'luaref-' .. cexpr:gsub("(.*)%(.*$", "%1")
   end
-  if fn ~= nil then
-    print('Looking up ' .. fn)
-    vim.cmd.help(fn)
+  if match ~= nil then
+    print('Looking up ' .. match)
+    vim.cmd.help(match)
   end
 end
