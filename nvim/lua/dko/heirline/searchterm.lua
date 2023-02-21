7492f116d58d5d811b6f9c4cce0b744393e835cd
David O'Trakoun <me@davidosomething.com>
2023-02-21 08:36:46 -0500

    count in searchterm

--- nvim/lua/dko/heirline/searchterm.lua
+++ nvim/lua/dko/heirline/searchterm.lua
@@ -1,12 +1,23 @@
 return {
   condition = function(self)
     self.contents = vim.fn.getreg("/")
+    self.count = vim.fn.searchcount({ recompute = 1, maxcount = -1 })
     return self.contents and string.len(self.contents) > 0
   end,
 
   {
     provider = " ? ",
     hl = "dkoStatusKey",
+  },
+
+  {
+    condition = function(self)
+      return self.count.current > 0
+    end,
+    provider = function(self)
+      return " " .. self.count.current .. "/" .. self.count.total .. " "
+    end,
+    hl = "dkoStatusValue",
   },
 
   {
