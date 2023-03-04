217cb9172a98a15a1460e89da8fb01e10d5dcbda
David O'Trakoun <me@davidosomething.com>
2023-03-03 22:10:38 -0500

    shorten cwd conditionally

--- nvim/lua/dko/heirline/cwd.lua
+++ nvim/lua/dko/heirline/cwd.lua
@@ -7,11 +7,21 @@ return {
   },
   {
     provider = function()
+      local uis = vim.api.nvim_list_uis()
+      local ui = uis[1] or { width = 80 }
+
+      local searchterm = vim.fn.getreg("/")
+      local extrachars = 20 + string.len(searchterm)
+      local remaining = ui.width - extrachars
+
       local cwd = vim.fn.getcwd(0)
       cwd = vim.fn.fnamemodify(cwd, ":~")
+      if string.len(cwd) < remaining then
+        return " " .. cwd .. " "
+      end
+
       cwd = vim.fn.pathshorten(cwd)
-      local trail = cwd:sub(-1) == "/" and "" or "/"
-      return " " .. cwd .. trail .. " "
+      return " " .. cwd .. " "
     end,
     hl = "dkoStatusValue",
   },
