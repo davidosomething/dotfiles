763fe40fc74f810aa69d5ebc8c89d58cebec3be7
David O'Trakoun <me@davidosomething.com>
2023-03-05 02:44:20 -0500

    indicate root

--- nvim/lua/dko/heirline/cwd.lua
+++ nvim/lua/dko/heirline/cwd.lua
@@ -1,27 +1,31 @@
 return {
+  init = function (self)
+    self.cwd = vim.fn.getcwd(0)
+  end,
   {
-    provider = function()
+    provider = function(self)
+      local is_project_root = vim.b['dko_project_root'] == self.cwd
+      if is_project_root then
+        return "  ʀᴏᴏᴛ "
+      end
       return "  "
     end,
     hl = "dkoStatusKey",
   },
   {
-    provider = function()
+    provider = function(self)
       local uis = vim.api.nvim_list_uis()
       local ui = uis[1] or { width = 80 }
 
       local searchterm = vim.fn.getreg("/")
-      local extrachars = 20 + string.len(searchterm)
+      local extrachars = 32 + string.len(searchterm)
       local remaining = ui.width - extrachars
 
-      local cwd = vim.fn.getcwd(0)
-      cwd = vim.fn.fnamemodify(cwd, ":~")
+      local cwd = vim.fn.fnamemodify(self.cwd, ":~")
       if string.len(cwd) < remaining then
         return " " .. cwd .. " "
       end
-
-      cwd = vim.fn.pathshorten(cwd)
-      return " " .. cwd .. " "
+      return " " .. vim.fn.pathshorten(cwd) .. " "
     end,
     hl = "dkoStatusValue",
   },
