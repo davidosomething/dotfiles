2e950738afa2eceee7b437dbd6cf6404caf5c3af
David O'Trakoun <dotrakoun@clearstreet.io>
2023-03-06 10:33:00 -0500

    just filter out help from special

--- nvim/lua/dko/heirline/statusline-special.lua
+++ nvim/lua/dko/heirline/statusline-special.lua
@@ -1,7 +1,9 @@
 return {
   condition = function()
     return require("heirline.conditions").buffer_matches({
-      buftype = require('dko.utils.buffer').SPECIAL_BUFTYPES,
+      buftype = vim.tbl_filter(function(bt)
+        return bt ~= "help"
+      end, require('dko.utils.buffer').SPECIAL_BUFTYPES),
       filetype = require('dko.utils.buffer').SPECIAL_FILETYPES,
     })
   end,
