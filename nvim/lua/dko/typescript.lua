d279ec2d752643c7b91563dcc1ece3908e0ab3d1
David O'Trakoun <dotrakoun@clearstreet.io>
2023-03-08 14:03:19 -0500

    get rid of typescript.nvim

--- nvim/lua/dko/typescript.lua
+++ nvim/lua/dko/typescript.lua
@@ -1,52 +1,41 @@
 local M = {}
 
-M.source_definition = function(bufnr)
-  local ok, executeCommandModule = pcall(require, "typescript.execute-command")
-  if not ok then
-    return false
-  end
-
-  local client = require('dko.lsp').get_active_client('tsserver')
+M.source_definition = function()
+  local client = require("dko.lsp").get_active_client("tsserver")
   if not client then
-    vim.notify('could not get tsserver', vim.log.levels.ERROR)
+    vim.notify("could not get tsserver", vim.log.levels.ERROR)
     return false
   end
 
-  local positionParams = vim.lsp.util.make_position_params(0, client.offset_encoding)
+  local positionParams =
+    vim.lsp.util.make_position_params(0, client.offset_encoding)
 
-  local commands = require("typescript.types.workspace-commands").WorkspaceCommands
-  local Methods = require("typescript.types.methods").Methods
-  local resolveHandler = require("typescript.utils").resolveHandler
-
-  local requestOk = executeCommandModule.executeCommand(
-    bufnr,
-    {
-      command = commands.GO_TO_SOURCE_DEFINITION,
-      arguments = {
-        positionParams.textDocument.uri,
-        positionParams.position
-      }
-    },
-    function(...)
-        local args = {...}
-        local handler = resolveHandler(bufnr, Methods.DEFINITION)
-        if not handler then
-            print("failed to go to source definition: could not resolve definition handler")
-            return
-        end
-        local res = args[2] or ({})
-        if vim.tbl_isempty(res) then
-          return false
-        end
-        vim.pretty_print(args)
-        handler(unpack(args))
+  local callback = function(...)
+    local args = { ... }
+    vim.pretty_print(args)
+    local res = args[2] or {}
+    if vim.tbl_isempty(res) then
+      return false
     end
-  )
 
-  if not requestOk then
-      print("failed to go to source definition: tsserver request failed")
+    local handler = client.handlers["textDocument/definition"]
+      or vim.lsp.handlers["textDocument/definition"]
+    if not handler then
+      print(
+        "failed to go to source definition: could not resolve definition handler"
+      )
+      return
+    end
+    handler(unpack(args))
   end
-  return requestOk
+
+  return client.request("workspace/executeCommand", {
+    command = "_typescript.goToSourceDefinition",
+    arguments = {
+      positionParams.textDocument.uri,
+      positionParams.position,
+    },
+  }, callback)
 end
 
 return M
