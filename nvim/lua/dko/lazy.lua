e5fc71704eba25ada8c4bdf2ac5e1d6533fa8c93
David O'Trakoun <me@davidosomething.com>
2023-04-12 00:59:13 -0400

    only checker for regular server session

--- nvim/lua/dko/lazy.lua
+++ nvim/lua/dko/lazy.lua
@@ -11,7 +11,7 @@ if not vim.loop.fs_stat(lazypath) then
 end
 vim.opt.rtp:prepend(lazypath)
 require("lazy").setup("dko.plugins", {
-  checker = { enabled = true },
+  checker = { enabled = string.find(vim.v.servername, "nvim.sock") },
   dev = {
     fallback = true,
     patterns = { "davidosomething" },
