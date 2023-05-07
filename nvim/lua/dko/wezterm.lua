23220bbe4e1a1d8a29746786faea91bf1ed97154
David O'Trakoun <me@davidosomething.com>
2023-05-07 19:50:20 -0400

    initial apply wezterm color setting

--- nvim/lua/dko/wezterm.lua
+++ nvim/lua/dko/wezterm.lua
@@ -17,28 +17,31 @@ M.monitor_colorscheme = function ()
     colorscheme_handle,
     colorscheme_file_path,
     {},
-    function ()
-      vim.loop.fs_open(colorscheme_file_path, "r", 438, function(_, fd)
-        if fd == nil then return end
-        vim.loop.fs_fstat(fd, function(_, stat)
-          if stat ==nil then return end
-          vim.loop.fs_read(fd, stat.size, 0, vim.schedule_wrap(function(_, data)
-            if data == "dark" then
-              vim.cmd('DKODark')
-            else
-              vim.cmd('DKOLight')
-            end
-            vim.loop.fs_close(fd)
-          end))
-        end)
-      end)
-    end
+    M.apply_darkmode_setting
   )
 
   return colorscheme_handle
 end
 
+M.apply_darkmode_setting = function ()
+  vim.loop.fs_open(colorscheme_file_path, "r", 438, function(_, fd)
+    if fd == nil then return end
+    vim.loop.fs_fstat(fd, function(_, stat)
+      if stat ==nil then return end
+      vim.loop.fs_read(fd, stat.size, 0, vim.schedule_wrap(function(_, data)
+        if data == "dark" then
+          vim.cmd('DKODark')
+        else
+          vim.cmd('DKOLight')
+        end
+        vim.loop.fs_close(fd)
+      end))
+    end)
+  end)
+end
+
 M.setup = function ()
+  M.apply_darkmode_setting()
   M.monitor_colorscheme()
 end
 
