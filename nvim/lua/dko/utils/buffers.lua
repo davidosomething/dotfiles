7817da38d5b240ba713fd061ba195e5340958073
David O'Trakoun <me@davidosomething.com>
2023-06-05 09:45:17 -0400

    don't filter loaded since i often open multi from shell

--- nvim/lua/dko/utils/buffers.lua
+++ nvim/lua/dko/utils/buffers.lua
@@ -4,7 +4,6 @@ local M = {}
 M.get_normal = function()
   return vim.tbl_filter(function(bufnr)
     return vim.bo[bufnr].buflisted
-      and vim.api.nvim_buf_is_loaded(bufnr)
       and not require("dko.utils.buffer").is_special(bufnr)
   end, vim.api.nvim_list_bufs())
 end
