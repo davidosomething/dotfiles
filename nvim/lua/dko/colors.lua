c42bf02f65b1f96e065de1efe9847b4a1e23e4de
David O'Trakoun <me@davidosomething.com>
2023-06-08 11:57:32 -0400

    cleanup colorscheme syncing and dynamic settings

--- nvim/lua/dko/colors.lua
+++ nvim/lua/dko/colors.lua
@@ -1,17 +1,75 @@
 local M = {}
 
+M.lightmode = function()
+  vim.o.bg = "light"
+  vim.cmd("colorscheme " .. require("dko.settings").get("colors.light"))
+end
+
+M.darkmode = function()
+  vim.o.bg = "dark"
+  vim.cmd("colorscheme " .. require("dko.settings").get("colors.dark"))
+end
+
 M.indent_blankline = function()
   if vim.g.colors_name == "meh" then
     vim.cmd([[
               highlight IndentBlanklineIndent2 guibg=#242424 gui=nocombine
               highlight IndentBlanklineContextChar guifg=#664422 gui=nocombine
             ]])
-  else
+  else -- two-firewatch
     vim.cmd([[
               highlight IndentBlanklineIndent2 guibg=#fafafa gui=nocombine
               highlight IndentBlanklineContextChar guifg=#eeeeee gui=nocombine
             ]])
   end
+end
+
+local colorscheme_file_path = os.getenv("XDG_STATE_HOME")
+  .. "/wezterm-colorscheme.txt"
+M.apply_from_file = function()
+  vim.loop.fs_open(colorscheme_file_path, "r", 438, function(_, fd)
+    if fd == nil then
+      return
+    end
+    vim.loop.fs_fstat(fd, function(_, stat)
+      if stat == nil then
+        return
+      end
+      vim.loop.fs_read(fd, stat.size, 0, function(_, data)
+        local nextmode = M[data .. "mode"]
+        if nextmode then
+          vim.schedule(nextmode)
+        end
+        vim.loop.fs_close(fd)
+      end)
+    end)
+  end)
+end
+
+local colorscheme_handle = nil
+M.monitor_colorscheme = function()
+  if colorscheme_handle ~= nil then
+    return colorscheme_handle
+  end
+
+  colorscheme_handle = vim.loop.new_fs_event()
+  if not colorscheme_handle then
+    return nil
+  end
+
+  vim.loop.fs_event_start(
+    colorscheme_handle,
+    colorscheme_file_path,
+    {},
+    M.apply_from_file
+  )
+
+  return colorscheme_handle
+end
+
+M.wezterm_sync = function()
+  M.apply_from_file()
+  M.monitor_colorscheme()
 end
 
 return M
--- nvim/lua/dko/settings.lua
+++ nvim/lua/dko/settings.lua
@@ -1,4 +1,8 @@
 local settings = {
+  colors = {
+    dark = 'meh',
+    light = 'two-firewatch',
+  },
   grepper = {
     ignore_file = vim.fn.expand("$DOTFILES") .. "/" .. "ag/dot.ignore",
   },
@@ -11,8 +15,27 @@ local settings = {
   },
 }
 
-return {
-  get = function(path)
-    return require("dko.utils.get")(settings, path)
-  end,
-}
+local M = {}
+
+M.watchers = {}
+
+M.get = function(path)
+  return require("dko.utils.object").get(settings, path)
+end
+
+M.set = function(path, value)
+  local current = M.get(path)
+  local success = require("dko.utils.object").set(settings, path, value)
+  if success and value ~= current then
+    local watchers = M.watchers[path]
+    for _, cb in pairs(watchers) do
+      cb({
+        path = path,
+        prev = current,
+        value = value,
+      })
+    end
+  end
+end
+
+return M
