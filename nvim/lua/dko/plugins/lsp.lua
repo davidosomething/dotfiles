3c62a321f47aa4de2aa34d4c128885d86ed069f9
David O'Trakoun <me@davidosomething.com>
2023-08-16 17:21:30 -0400

    wip - null-ls to efm

--- nvim/lua/dko/plugins/lsp.lua
+++ nvim/lua/dko/plugins/lsp.lua
@@ -128,18 +128,29 @@ return {
       -- border on :LspInfo window
       require("lspconfig.ui.windows").default_options.border = "rounded"
 
+      local lspconfig = require("lspconfig")
+
       -- =====================================================================
-      -- manual lsp setup
-      -- The following are LSPs that are not managed by mason-lspconfig
+      -- The following are LSPs that are not managed by mason-lspconfig. I.e.
+      -- not present in
+      -- https://github.com/williamboman/mason-lspconfig.nvim/tree/main/lua/mason-lspconfig/server_configurations
       -- =====================================================================
-      require("lspconfig").dartls.setup({
+
+      -- =====================================================================
+      -- dart - dart_ls
+      -- =====================================================================
+
+      lspconfig.dartls.setup({
         settings = {
-          dart = {
-            showTodos = false,
-          },
+          dart = { showTodos = false },
         },
       })
-      require("lspconfig").tilt_ls.setup({})
+
+      -- =====================================================================
+      -- tiltfile - tilt_ls
+      -- =====================================================================
+
+      lspconfig.tilt_ls.setup({})
     end,
   },
 
@@ -228,208 +239,200 @@ return {
       local handlers = {
         function(server)
           lspconfig[server].setup(with_lsp_capabilities())
-        end,
+        end
+      }
+      handlers["cssmodules_ls"] = function()
+        lspconfig.cssmodules_ls.setup(with_lsp_capabilities({
+          ---note: local on_attach happens AFTER autocmd LspAttach
+          ---@param client table
+          on_attach = function(client)
+            -- https://github.com/davidosomething/dotfiles/issues/521
+            -- https://github.com/antonk52/cssmodules-language-server#neovim
+            -- avoid accepting `definitionProvider` responses from this LSP
+            client.server_capabilities.definitionProvider = false
+          end,
+        }))
+      end
 
-        ["cssmodules_ls"] = function()
-          lspconfig.cssmodules_ls.setup(with_lsp_capabilities({
-            ---note: local on_attach happens AFTER autocmd LspAttach
-            ---@param client table
-            on_attach = function(client)
-              -- https://github.com/davidosomething/dotfiles/issues/521
-              -- https://github.com/antonk52/cssmodules-language-server#neovim
-              -- avoid accepting `definitionProvider` responses from this LSP
-              client.server_capabilities.definitionProvider = false
-            end,
-          }))
-        end,
-
-        ["docker_compose_language_service"] = function()
-          lspconfig.docker_compose_language_service.setup(
-            with_lsp_capabilities({
-              on_attach = function(client)
-                -- stylua or NOTHING
-                client.server_capabilities.documentFormattingProvider = false
-                client.server_capabilities.documentRangeFormattingProvider =
-                  false
-              end,
-            })
-          )
-        end,
-
-        ["jsonls"] = function()
-          lspconfig.jsonls.setup(with_lsp_capabilities({
-            settings = {
-              json = {
-                schemas = require("schemastore").json.schemas(),
-                -- https://github.com/b0o/SchemaStore.nvim/issues/8#issuecomment-1129528787
-                validate = { enable = true },
-              },
-            },
-          }))
-        end,
-
-        ["lua_ls"] = function()
-          lspconfig.lua_ls.setup(with_lsp_capabilities({
-            ---@param client table
+      handlers["docker_compose_language_service"] = function()
+        lspconfig.docker_compose_language_service.setup(
+          with_lsp_capabilities({
             on_attach = function(client)
               -- stylua or NOTHING
               client.server_capabilities.documentFormattingProvider = false
-              client.server_capabilities.documentRangeFormattingProvider = false
+              client.server_capabilities.documentRangeFormattingProvider =
+                false
             end,
+          })
+        )
+      end
 
-            -- no more neodev https://github.com/neovim/neovim/pull/24592
-            on_init = function(client)
-              local path = client.workspace_folders[1].name
-              if
-                not vim.loop.fs_stat(path .. "/.luarc.json")
-                and not vim.loop.fs_stat(path .. "/.luarc.jsonc")
-              then
-                client.config.settings =
-                  vim.tbl_deep_extend("force", client.config.settings.Lua, {
-                    runtime = {
-                      -- Tell the language server which version of Lua you're using (most likely LuaJIT in the case of Neovim)
-                      version = "LuaJIT",
-                    },
-                    -- Make the server aware of Neovim runtime files
-                    workspace = {
-                      library = { vim.env.VIMRUNTIME },
-                      -- or pull in all of 'runtimepath'. NOTE: this is a lot slower
-                      --library = vim.api.nvim_get_runtime_file("", true),
-                    },
-                  })
-                client.notify(
-                  "workspace/didChangeConfiguration",
-                  { settings = client.config.settings }
-                )
-              end
-              return true
-            end,
+      handlers["jsonls"] = function()
+        lspconfig.jsonls.setup(with_lsp_capabilities({
+          settings = {
+            json = {
+              schemas = require("schemastore").json.schemas(),
+              -- https://github.com/b0o/SchemaStore.nvim/issues/8#issuecomment-1129528787
+              validate = { enable = true },
+            },
+          },
+        }))
+      end
 
-            settings = {
-              Lua = {
-                format = { enable = false },
-                hint = { enable = true },
-                workspace = {
-                  maxPreload = 1000,
-                  preloadFileSize = 500,
-                  checkThirdParty = false,
-                },
+      handlers["lua_ls"] = function()
+        lspconfig.lua_ls.setup(with_lsp_capabilities({
+          on_attach = function(client)
+            -- stylua or NOTHING
+            client.server_capabilities.documentFormattingProvider = false
+            client.server_capabilities.documentRangeFormattingProvider = false
+          end,
+
+          -- no more neodev https://github.com/neovim/neovim/pull/24592
+          on_init = function(client)
+            local path = client.workspace_folders[1].name
+            if
+              not vim.loop.fs_stat(path .. "/.luarc.json")
+              and not vim.loop.fs_stat(path .. "/.luarc.jsonc")
+            then
+              client.config.settings =
+                vim.tbl_deep_extend("force", client.config.settings.Lua, {
+                  runtime = {
+                    -- Tell the language server which version of Lua you're using (most likely LuaJIT in the case of Neovim)
+                    version = "LuaJIT",
+                  },
+                  -- Make the server aware of Neovim runtime files
+                  workspace = {
+                    library = { vim.env.VIMRUNTIME },
+                    -- or pull in all of 'runtimepath'. NOTE: this is a lot slower
+                    --library = vim.api.nvim_get_runtime_file("", true),
+                  },
+                })
+              client.notify(
+                "workspace/didChangeConfiguration",
+                { settings = client.config.settings }
+              )
+            end
+            return true
+          end,
+
+          settings = {
+            Lua = {
+              format = { enable = false },
+              hint = { enable = true },
+              workspace = {
+                maxPreload = 1000,
+                preloadFileSize = 500,
+                checkThirdParty = false,
               },
             },
-          }))
-        end,
+          },
+        }))
+      end
 
-        ["stylelint_lsp"] = function()
-          lspconfig.stylelint_lsp.setup(with_lsp_capabilities({
-            -- Disable on some filetypes
-            -- https://github.com/neovim/nvim-lspconfig/blob/master/lua/lspconfig/server_configurations/stylelint_lsp.lua
-            filetypes = {
-              "css",
-              "less",
-              "scss",
-              "sugarss",
-              -- 'vue',
-              "wxss",
-              -- 'javascript',
-              -- 'javascriptreact',
-              -- 'typescript',
-              -- 'typescriptreact',
-            },
-          }))
-        end,
+      handlers["stylelint_lsp"] = function()
+        lspconfig.stylelint_lsp.setup(with_lsp_capabilities({
+          -- Disable on some filetypes
+          -- https://github.com/neovim/nvim-lspconfig/blob/master/lua/lspconfig/server_configurations/stylelint_lsp.lua
+          filetypes = {
+            "css",
+            "less",
+            "scss",
+            "sugarss",
+            -- 'vue',
+            "wxss",
+            -- 'javascript',
+            -- 'javascriptreact',
+            -- 'typescript',
+            -- 'typescriptreact',
+          },
+        }))
+      end
 
-        ["tsserver"] = function()
-          lspconfig.tsserver.setup(with_lsp_capabilities({
-            ---@param _ table client
-            ---@param bufnr number
-            on_attach = function(_, bufnr)
-              require("dko.mappings").bind_tsserver_lsp(bufnr)
-            end,
+      handlers["tsserver"] = function()
+        local inlay_hint_settings = {
+          includeInlayParameterNameHints = "all",
+          includeInlayParameterNameHintsWhenArgumentMatchesName = false,
+          includeInlayFunctionParameterTypeHints = true,
+          includeInlayVariableTypeHints = true,
+          includeInlayVariableTypeHintsWhenTypeMatchesName = false,
+          includeInlayPropertyDeclarationTypeHints = true,
+          includeInlayFunctionLikeReturnTypeHints = true,
+          includeInlayEnumMemberValueHints = true,
+        }
 
-            handlers = {
-              ["textDocument/publishDiagnostics"] = function(
+        lspconfig.tsserver.setup(with_lsp_capabilities({
+          ---@param _ table client
+          ---@param bufnr number
+          on_attach = function(_, bufnr)
+            require("dko.mappings").bind_tsserver_lsp(bufnr)
+          end,
+
+          handlers = {
+            ["textDocument/publishDiagnostics"] = function(
+              _,
+              result,
+              ctx,
+              config
+            )
+              if not result.diagnostics then
+                return
+              end
+
+              -- ignore some tsserver diagnostics
+              local idx = 1
+              while idx <= #result.diagnostics do
+                local entry = result.diagnostics[idx]
+
+                local formatter = require("format-ts-errors")[entry.code]
+                entry.message = formatter and formatter(entry.message)
+                  or entry.message
+
+                -- codes: https://github.com/microsoft/TypeScript/blob/main/src/compiler/diagnosticMessages.json
+                if entry.code == 80001 then
+                  -- { message = "File is a CommonJS module; it may be converted to an ES module.", }
+                  table.remove(result.diagnostics, idx)
+                else
+                  idx = idx + 1
+                end
+              end
+
+              vim.lsp.diagnostic.on_publish_diagnostics(
                 _,
                 result,
                 ctx,
                 config
               )
-                if result.diagnostics == nil then
-                  return
-                end
+            end,
+          },
 
-                -- ignore some tsserver diagnostics
-                local idx = 1
-                while idx <= #result.diagnostics do
-                  local entry = result.diagnostics[idx]
-
-                  local formatter = require("format-ts-errors")[entry.code]
-                  entry.message = formatter and formatter(entry.message)
-                    or entry.message
-
-                  -- codes: https://github.com/microsoft/TypeScript/blob/main/src/compiler/diagnosticMessages.json
-                  if entry.code == 80001 then
-                    -- { message = "File is a CommonJS module; it may be converted to an ES module.", }
-                    table.remove(result.diagnostics, idx)
-                  else
-                    idx = idx + 1
-                  end
-                end
-
-                vim.lsp.diagnostic.on_publish_diagnostics(
-                  _,
-                  result,
-                  ctx,
-                  config
-                )
-              end,
+          settings = {
+            typescript = {
+              inlayHints = inlay_hint_settings,
             },
+            javascript = {
+              inlayHints = inlay_hint_settings,
+            },
+          },
+        }))
+      end
 
-            settings = {
-              typescript = {
-                inlayHints = {
-                  includeInlayParameterNameHints = "all",
-                  includeInlayParameterNameHintsWhenArgumentMatchesName = false,
-                  includeInlayFunctionParameterTypeHints = true,
-                  includeInlayVariableTypeHints = true,
-                  includeInlayVariableTypeHintsWhenTypeMatchesName = false,
-                  includeInlayPropertyDeclarationTypeHints = true,
-                  includeInlayFunctionLikeReturnTypeHints = true,
-                  includeInlayEnumMemberValueHints = true,
+      handlers["yamlls"] = function()
+        lspconfig.yamlls.setup(with_lsp_capabilities({
+          settings = {
+            yaml = {
+              format = { enable = false }, -- prefer yamlfmt
+              validate = { enable = false }, -- prefer yamllint
+              -- disable built-in fetch schemas, prefer schemastore.nvim
+              schemaStore = { enable = false },
+              schemas = require("schemastore").yaml.schemas({
+                ignore = {
+                  "Cheatsheets",
                 },
-              },
-              javascript = {
-                inlayHints = {
-                  includeInlayParameterNameHints = "all",
-                  includeInlayParameterNameHintsWhenArgumentMatchesName = false,
-                  includeInlayFunctionParameterTypeHints = true,
-                  includeInlayVariableTypeHints = true,
-                  includeInlayVariableTypeHintsWhenTypeMatchesName = false,
-                  includeInlayPropertyDeclarationTypeHints = true,
-                  includeInlayFunctionLikeReturnTypeHints = true,
-                  includeInlayEnumMemberValueHints = true,
-                },
-              },
+              }),
             },
-          }))
-        end,
-
-        ["yamlls"] = function()
-          lspconfig.yamlls.setup(with_lsp_capabilities({
-            settings = {
-              yaml = {
-                format = { enable = false }, -- prefer yamlfmt
-                validate = { enable = false }, -- prefer yamllint
-                -- disable built-in fetch schemas, prefer schemastore.nvim
-                schemaStore = { enable = false },
-                schemas = require("schemastore").yaml.schemas({
-                  ignore = {
-                    "Cheatsheets",
-                  },
-                }),
-              },
-            },
-          }))
-        end,
+          },
+        }))
+      end
       }
 
       require("mason-lspconfig").setup({
