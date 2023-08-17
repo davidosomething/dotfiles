f729cf1fbbfa5174b9d557f6449bebeedd66f8e6
David O'Trakoun <dotrakoun@clearstreet.io>
2023-08-17 11:24:00 -0400

    fix bufnr, enable sh treesitter, filename display stepping

--- nvim/lua/dko/treesitter.lua
+++ nvim/lua/dko/treesitter.lua
@@ -1,7 +1,7 @@
 local M = {}
 
 M.is_highlight_enabled = function(bufnr)
-  bufnr = vim.fn.bufnr(bufnr or 0)
+  bufnr = bufnr and vim.fn.bufnr(bufnr) or vim.fn.bufnr()
   return bufnr > -1 and vim.treesitter.highlighter.active[bufnr] ~= nil
 end
 
