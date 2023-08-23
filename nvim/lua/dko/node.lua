5c5ce01e57295dd419de59d3e1c3eec4c6794056
David O'Trakoun <dotrakoun@clearstreet.io>
2023-08-23 01:39:52 -0400

    b not bo

--- nvim/lua/dko/node.lua
+++ nvim/lua/dko/node.lua
@@ -1,7 +1,7 @@
 local M = {}
 
 M.get_node_modules_dir = function(opts)
-  if vim.bo.node_modules_dir == nil then
+  if vim.b.node_modules_dir == nil then
     local find_opts = vim.tbl_extend("force", {
       limit = 1,
       path = vim.fs.dirname(vim.api.nvim_buf_get_name(0)),
@@ -9,22 +9,22 @@ M.get_node_modules_dir = function(opts)
       upward = true,
     }, opts or {})
     local res = vim.fs.find("node_modules", find_opts)
-    vim.bo.node_modules_dir = res[1] or false
+    vim.b.node_modules_dir = res[1] or false
   end
-  return vim.bo.node_modules_dir
+  return vim.b.node_modules_dir
 end
 
 M.get_eslint = function()
-  if vim.bo.eslint == nil then
+  if vim.b.eslint == nil then
     local nmd = M.get_node_modules_dir()
     if nmd == false then
       return false
     end
     local eslint_path = vim.fs.joinpath(nmd, ".bin/eslint")
-    vim.bo.eslint = require("dko.utils.file").find_exists({ eslint_path })
+    vim.b.eslint = require("dko.utils.file").find_exists({ eslint_path })
       or false
   end
-  return vim.bo.eslint
+  return vim.b.eslint
 end
 
 ---@return table|nil -- lua table
