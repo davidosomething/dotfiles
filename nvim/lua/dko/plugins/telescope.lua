fe25f86407e3409105248116e4816feaf7feb42d
David O'Trakoun <me@davidosomething.com>
2023-08-28 11:30:25 -0400

    bump telescope

--- nvim/lua/dko/plugins/telescope.lua
+++ nvim/lua/dko/plugins/telescope.lua
@@ -13,7 +13,7 @@ return {
 
   {
     "nvim-telescope/telescope.nvim",
-    tag = "0.1.1",
+    tag = "0.1.2",
     dependencies = {
       "nvim-lua/plenary.nvim",
       { "nvim-telescope/telescope-fzf-native.nvim", build = "make" },
