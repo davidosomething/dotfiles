67043b8ace89a1a3c36c0cf223182fd99daeebcb
David O'Trakoun <dotrakoun@clearstreet.io>
2023-08-29 11:36:35 -0400

    fix prettier detection

--- nvim/lua/dko/format.lua
+++ nvim/lua/dko/format.lua
@@ -22,13 +22,13 @@ local format_jsts = function()
   if vim.b.has_eslint then
     if vim.b.has_eslint_plugin_prettier == nil then
       vim.b.has_eslint_plugin_prettier =
-        require("dko.node").has_eslint_plugin("prettier/prettier")
+        require("dko.node").has_eslint_plugin("prettier")
     end
 
     vim.cmd.EslintFixAll()
 
     if vim.b.has_eslint_plugin_prettier then
-      vim.notify("format with eslint+prettier/prettier", vim.log.levels.INFO, {
+      vim.notify("format with eslint-plugin-prettier", vim.log.levels.INFO, {
         render = "compact",
         title = "LSP > eslint",
       })
