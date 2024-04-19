590e5a9fdf5cf5d3cc478fb5cd06336c633b6f7b
David O'Trakoun <dotrakoun@clearstreet.io>
2024-04-18 20:02:37 -0400

    isremote more lenient

--- nvim/lua/dko/utils/vte.lua
+++ nvim/lua/dko/utils/vte.lua
@@ -5,8 +5,7 @@ local M = {}
 M.is_remote = function()
   local is_remote = vim.env.SSH_CLIENT or vim.uv.fs_stat("/.dockerenv")
   -- yes .dockerenv is in root /
-  return (is_remote and (vim.env.DISPLAY == nil or vim.env.DISPLAY == ""))
-    or vim.env.NVIM_INSTALL_ALL
+  return is_remote or vim.env.NVIM_INSTALL_ALL
 end
 
 return M
