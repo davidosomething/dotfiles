708a856d575176e4650e4309e94d2b97ba5030ef
David O'Trakoun <me@davidosomething.com>
2024-06-13 11:11:10 -0400

    boolean only

--- nvim/lua/dko/utils/vte.lua
+++ nvim/lua/dko/utils/vte.lua
@@ -3,9 +3,10 @@
 local M = {}
 
 M.is_remote = function()
-  local is_remote = vim.env.SSH_CLIENT or vim.uv.fs_stat("/.dockerenv")
+  local is_remote = vim.env.SSH_CLIENT ~= nil
+    or vim.uv.fs_stat("/.dockerenv") ~= nil
   -- yes .dockerenv is in root /
-  return is_remote or vim.env.NVIM_INSTALL_ALL
+  return is_remote or vim.env.NVIM_INSTALL_ALL ~= nil
 end
 
 return M
