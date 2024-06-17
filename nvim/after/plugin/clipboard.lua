6f1c031b2b6f926b08627dd21da01512c1cde4b6
David O'Trakoun <me@davidosomething.com>
2024-06-16 22:52:01 -0400

    disable docker exec osc52

--- nvim/after/plugin/clipboard.lua
+++ nvim/after/plugin/clipboard.lua
@@ -10,19 +10,20 @@ if require("dko.utils.clipboard").should_use_osc52() then
   end
   -- else it should automatically use OSC 52 according to :h clipboard-osc52
   -- BUT for docker exec shells we need to manually enable
-  if require("dko.utils.vte").is_docker_exec() then
-    vim.g.clipboard = {
-      name = "OSC 52 (docker)",
-      copy = {
-        ["+"] = require("vim.ui.clipboard.osc52").copy("+"),
-        ["*"] = require("vim.ui.clipboard.osc52").copy("*"),
-      },
-      paste = {
-        ["+"] = require("vim.ui.clipboard.osc52").paste("+"),
-        ["*"] = require("vim.ui.clipboard.osc52").paste("*"),
-      },
-    }
-  end
+  -- Not working
+  -- if require("dko.utils.vte").is_docker_exec() then
+  --   vim.g.clipboard = {
+  --     name = "OSC 52 (docker)",
+  --     copy = {
+  --       ["+"] = require("vim.ui.clipboard.osc52").copy("+"),
+  --       ["*"] = require("vim.ui.clipboard.osc52").copy("*"),
+  --     },
+  --     paste = {
+  --       ["+"] = require("vim.ui.clipboard.osc52").paste("+"),
+  --       ["*"] = require("vim.ui.clipboard.osc52").paste("*"),
+  --     },
+  --   }
+  -- end
 else
   vim.o.clipboard = "unnamedplus"
 end
