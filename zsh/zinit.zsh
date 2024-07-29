fcac8ef45c9d6f2689c93e80d8200fd7881fd3b9
David O'Trakoun <dotrakoun@clearstreet.io>
2024-07-29 09:21:17 -0400

    use full fzf via zinit, add delta completions

--- zsh/zinit.zsh
+++ zsh/zinit.zsh
@@ -47,14 +47,6 @@ function {
     ;
 
   # ----------------------------------------------------------------------------
-  # FZF
-  # ----------------------------------------------------------------------------
-
-  zinit lucid for \
-    if'! __dko_has fzf' from'gh-r' as'program' \
-    'junegunn/fzf-bin';
-
-  # ----------------------------------------------------------------------------
   # Utilities
   # ----------------------------------------------------------------------------
 
@@ -78,6 +70,8 @@ function {
     atload"$bat_manpager" \
     '@sharkdp/bat' \
     \
+    atclone'delta/delta --generate-completion zsh > delta/_delta' \
+    atpull'%atclone' \
     mv'delta* -> delta' \
     pick'delta/delta' \
     'dandavison/delta' \
@@ -86,6 +80,9 @@ function {
     atclone"cp -vf fd/fd.1 \"${man_dir}\"" \
     atpull'%atclone' \
     '@sharkdp/fd' \
+    \
+    atload'source <(fzf --zsh)' \
+    'junegunn/fzf';
     \
     mv'jq* -> jq' \
     'jqlang/jq' \
