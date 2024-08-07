97dd1b8e84d92eead79b9ee56f14ef6ccb9fe695
David O'Trakoun <dotrakoun@clearstreet.io>
2024-08-07 10:20:57 -0400

    disable yanky if osc clipboard

--- nvim/lua/dko/settings.lua
+++ nvim/lua/dko/settings.lua
@@ -19,8 +19,8 @@ local settings = {
   lsp = {
     -- Which code action UI should we try first?
     -- The alternative will be tried second
-    code_action = "tiny-code-action",
-    --code_action = 'actions-preview',
+    --code_action = "tiny-code-action",
+    code_action = "actions-preview",
   },
 }
 
