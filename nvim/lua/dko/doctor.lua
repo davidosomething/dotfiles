182acf774a87ce40c49ae342610b330bceabee5f
David O'Trakoun <me@davidosomething.com>
2024-10-05 02:09:34 -0400

    doctor commands

--- nvim/lua/dko/doctor.lua
+++ nvim/lua/dko/doctor.lua
@@ -5,20 +5,68 @@ local M = {}
 M.warnings = {}
 M.errors = {}
 
+--- Add an error
 M.error = function(params)
   table.insert(M.errors, params)
 end
 
+--- Add a warning
 M.warn = function(params)
   table.insert(M.warnings, params)
 end
 
-vim.api.nvim_create_user_command("DKODoctorErrors", function()
-  vim.cmd.Bufferize("lua vim.print(require('dko.doctor').errors)")
-end, { desc = "Show DKODoctor errors" })
+local tobuf = function(lines)
+  vim.cmd("new")
+  vim.api.nvim_buf_set_lines(0, 0, -1, false, lines)
+  vim.opt_local.buftype = "nofile"
+  vim.opt_local.filetype = "lua"
+  vim.opt_local.readonly = true
+  vim.opt_local.modified = false
+end
 
-vim.api.nvim_create_user_command("DKODoctorWarnings", function()
-  vim.cmd.Bufferize("lua vim.print(require('dko.doctor').warnings)")
-end, { desc = "Show DKODoctor warnings" })
+local subcommands = {
+  errors = function()
+    tobuf(
+      vim.split(
+        vim.inpsect({ errors = require("dko.doctor").errors }),
+        "\n",
+        { plain = true }
+      )
+    )
+  end,
+  warnings = function()
+    tobuf(
+      vim.split(
+        vim.inspect({ warnings = require("dko.doctor").warnings }),
+        "\n",
+        { plain = true }
+      )
+    )
+  end,
+  all = function()
+    tobuf(vim.split(
+      vim.inspect({
+        errors = require("dko.doctor").errors,
+        warnings = require("dko.doctor").warnings,
+      }),
+      "\n",
+      { plain = true }
+    ))
+  end,
+}
+
+---@return string[]
+local complete = function(--[[ ArgLead, CmdLine, CursorPos ]])
+  return vim.tbl_keys(subcommands)
+end
+
+vim.api.nvim_create_user_command("DKODoctor", function(opts)
+  local handler = subcommands[opts.args] or subcommands.all
+  handler()
+end, {
+  desc = "DKODoctor errors and warnings",
+  nargs = "*",
+  complete = complete,
+})
 
 return M
