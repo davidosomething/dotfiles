d0f76d009ce2915896c821f21d198206abbc31a7
David O'Trakoun <me@davidosomething.com>
2024-12-11 22:28:28 -0500

    lazy use main

--- nvim/lua/dko/lazy.lua
+++ nvim/lua/dko/lazy.lua
@@ -1,7 +1,13 @@
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if vim.fn.getftype(lazypath) ~= "dir" then
   local lazyrepo = "https://github.com/folke/lazy.nvim.git"
-  local out = vim.fn.system({ "git", "clone", "--filter=blob:none", "--branch=stable", lazyrepo, lazypath })
+  local out = vim.fn.system({
+    "git",
+    "clone",
+    "--filter=blob:none",
+    lazyrepo,
+    lazypath,
+  })
   if vim.v.shell_error ~= 0 then
     vim.api.nvim_echo({
       { "Failed to clone lazy.nvim:\n", "ErrorMsg" },
