1f9c727c22fad7fb104b2389d43d110d2f4e90ba
David O'Trakoun <me@davidosomething.com>
2024-12-13 10:57:34 -0500

    disable stylelint

--- nvim/lua/dko/tools/css.lua
+++ nvim/lua/dko/tools/css.lua
@@ -1,27 +1,28 @@
 local tools = require("dko.tools")
 
-tools.register({
-  name = "stylelint_lsp",
-  mason_type = "lsp",
-  require = "npm",
-  runner = "mason-lspconfig",
-  lspconfig = function()
-    ---@type lspconfig.Config
-    return {
-      -- Disable on some filetypes
-      -- https://github.com/neovim/nvim-lspconfig/blob/master/lua/lspconfig/server_configurations/stylelint_lsp.lua
-      filetypes = {
-        "css",
-        "less",
-        "scss",
-        "sugarss",
-        -- 'vue',
-        "wxss",
-        -- 'javascript',
-        -- 'javascriptreact',
-        -- 'typescript',
-        -- 'typescriptreact',
-      },
-    }
-  end,
-})
+-- stylelint not working any more
+-- tools.register({
+--   name = "stylelint_lsp",
+--   mason_type = "lsp",
+--   require = "npm",
+--   runner = "mason-lspconfig",
+--   lspconfig = function()
+--     ---@type lspconfig.Config
+--     return {
+--       -- Disable on some filetypes
+--       -- https://github.com/neovim/nvim-lspconfig/blob/master/lua/lspconfig/server_configurations/stylelint_lsp.lua
+--       filetypes = {
+--         "css",
+--         "less",
+--         "scss",
+--         "sugarss",
+--         -- 'vue',
+--         "wxss",
+--         -- 'javascript',
+--         -- 'javascriptreact',
+--         -- 'typescript',
+--         -- 'typescriptreact',
+--       },
+--     }
+--   end,
+-- })
