c4d0501245ec32565817596db2b8e0d509468008
David O'Trakoun <me@davidosomething.com>
2024-12-12 19:57:18 -0500

    move git to commands, fix anno, comments, back to dressing

--- nvim/lua/dko/mappings.lua
+++ nvim/lua/dko/mappings.lua
@@ -318,14 +318,13 @@ map("i", "<S-Tab>", "<C-d>", {
 -- Diagnostic mappings
 -- ===========================================================================
 
--- @TODO remove - default as of https://github.com/neovim/neovim/commit/73034611c25d16df5e87c8afb2d339a03a91bd0d/
+-- override default mapping with float enabled
 map("n", "[d", function()
   vim.diagnostic.goto_prev({
     float = dkosettings.get("diagnostics.goto_float"),
   })
 end, { desc = "Go to prev diagnostic and open float" })
-
--- @TODO remove - default as of https://github.com/neovim/neovim/commit/73034611c25d16df5e87c8afb2d339a03a91bd0d/
+-- override default mapping with float enabled
 map("n", "]d", function()
   vim.diagnostic.goto_next({
     float = dkosettings.get("diagnostics.goto_float"),
