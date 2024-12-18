dc1fb6d16cfe39ca70d2a8d031c3e1590bb5c985
David O'Trakoun <me@davidosomething.com>
2024-12-18 18:22:42 -0500

    hyper+, for prefs

--- mac/hammerspoon/launcher/apps.lua
+++ mac/hammerspoon/launcher/apps.lua
@@ -10,7 +10,7 @@ hs.hotkey.bind(hyper, 'b', function()
 end)
 
 -- C is for caffeine
---
+
 -- d is for lunette right
 
 -- f for lunette
@@ -22,11 +22,16 @@ end)
 -- h for lunette
 
 hs.hotkey.bind(hyper, 'n', function()
-  hs.application.launchOrFocus("Joplin")
+  hs.application.launchOrFocus("Standard Notes")
 end)
 
 hs.hotkey.bind(hyper, 's', function()
   hs.application.launchOrFocus("Slack")
 end)
 
-return nil
+hs.hotkey.bind(hyper, ',', function()
+  local url = 'x-apple.systempreferences:com.apple.Software-Update-Settings.extension'
+  local scheme = url:match('^([^:]+)')
+  local handler = hs.urlevent.getDefaultHandler(scheme)
+  hs.urlevent.openURLWithBundle(url, handler)
+end)
