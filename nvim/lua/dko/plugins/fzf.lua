06cb93aa1166c6ad2213bc86bacac29265202131
David O'Trakoun <dotrakoun@clearstreet.io>
2024-12-19 01:44:47 -0500

    customize fzf preview/input position to fit on smaller screen

--- nvim/lua/dko/plugins/fzf.lua
+++ nvim/lua/dko/plugins/fzf.lua
@@ -7,6 +7,16 @@ return {
     dependencies = "echasnovski/mini.icons",
     config = function()
       require("fzf-lua").setup({
+        fzf_opts = { ["--layout"] = "reverse-list" }, -- input goes below the list
+        winopts = {
+          height = 0.90, -- window height
+          width = 0.90, -- window width
+          preview = {
+            ---@type 'horizontal'|'vertical'|'flex'
+            layout = "vertical",
+            vertical = "up:70%", -- up|down:size -- preview goes above the list
+          },
+        },
         oldfiles = {
           include_current_session = true,
           stat_file = true, -- verify files exist on disk
