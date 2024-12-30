3e1ea670e5316aa97f0e68da17b412c8dde207d3
David O'Trakoun <dotrakoun@clearstreet.io>
2024-12-30 11:41:17 -0500

    extract path fit and fix relative path up

--- nvim/lua/dko/utils/path.lua
+++ nvim/lua/dko/utils/path.lua
@@ -1,5 +1,30 @@
 local M = {}
 
+---Run pathshorten at decreasing lengths until fits in space
+---@param space integer
+---@param path string
+---@return string -- shortened to fit space, or down to 1-char segments
+M.fit = function(space, path)
+  local segments = vim.split(path, "/")
+
+  local longest = 0
+  for _, segment in ipairs(segments) do
+    longest = segment:len() > longest and segment:len() or longest
+  end
+
+  local final
+  local len = longest
+  while len > 0 and type(final) ~= "string" do
+    local attempt = vim.fn.pathshorten(path, len)
+    final = attempt:len() < space and attempt
+    len = len - 1
+  end
+  if not final then
+    final = vim.fn.pathshorten(path, 1)
+  end
+  return final
+end
+
 ---Find the common root between two paths
 ---@param a string
 ---@param b string
