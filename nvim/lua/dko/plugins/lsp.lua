402dd101adb46d3bd6dddfd245f7842eba1b0d73
David O'Trakoun <me@davidosomething.com>
2025-03-07 00:45:11 -0500

    check for node

--- nvim/lua/dko/plugins/lsp.lua
+++ nvim/lua/dko/plugins/lsp.lua
@@ -185,7 +185,7 @@ return {
   {
     "neoclide/coc.nvim",
     branch = "release",
-    cond = has_ui and dkosettings.get("use_coc"),
+    cond = has_ui and dkosettings.get("use_coc") and vim.fn.executable("node"),
     init = function()
       vim.g.coc_start_at_startup = true
       vim.g.coc_global_extensions = {
