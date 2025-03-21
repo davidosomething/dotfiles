103c3fcb0dc7f90669d70476ced771fbde69f93a
David O'Trakoun <me@davidosomething.com>
2025-03-21 18:09:21 -0400

    refactor fill

--- nvim/lua/dko/utils/hr.lua
+++ nvim/lua/dko/utils/hr.lua
@@ -3,30 +3,35 @@ local M = {}
 -- Generate a line of pattern pat end of line to &textwidth
 ---@param pat string
 M.fill = function(pat)
-  local patlen = pat:len()
-  local lastcol = vim.fn.col("$")
-  -- how many characters left from end of line to textwidth
-  local available = math.max(vim.bo.textwidth - (lastcol - 2), 0)
-  -- how many repititions of pat can we append before hitting &textwidth
-  local reps = math.floor(available / patlen) - 1
-  if reps < 1 then
+  ---@TODO make this an arg?
+  local opts = { right_padding = 2 }
+
+  local target_col = vim.bo.textwidth - opts.right_padding
+  if target_col <= 0 then
+    -- bad right_padding
     return
   end
 
-  -- Insert a space if the line is dirty and doesn't already end with
-  -- whitespace
-  local last_char = (vim.api.nvim_get_current_line()):sub(-1)
+  local current_line = vim.api.nvim_get_current_line()
+
+  local filled_line = current_line
+  local last_char = filled_line:sub(-1)
+  local is_empty = filled_line == ""
   local is_end_space = last_char == " " or last_char == "\t"
-  if lastcol > 1 and not is_end_space then
-    vim.cmd("normal! 1A ")
-    reps = math.floor((available - 1) / patlen) - 1
-    if reps < 1 then
-      return
-    end
+  if not is_empty and not is_end_space then
+    filled_line = filled_line .. " "
+  end
+  while filled_line:len() + pat:len() <= target_col do
+    filled_line = filled_line .. pat
+  end
+  if current_line == filled_line then
+    -- no change, no-op
+    return
   end
 
-  -- Insert the rule, go to next line
-  vim.cmd("normal! " .. reps .. "A" .. pat)
+  -- apply change
+  vim.api.nvim_set_current_line(filled_line)
+  -- go to the next line
   vim.api.nvim_feedkeys("o", "nt", true)
 end
 
