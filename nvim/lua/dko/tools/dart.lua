8501fc2fcad430c2970a29c17530b046b07eb103
David O'Trakoun <me@davidosomething.com>
2025-05-14 21:44:37 -0400

    remove redundant dart lsp config

--- nvim/lua/dko/tools/dart.lua
+++ nvim/lua/dko/tools/dart.lua
@@ -3,12 +3,4 @@ local tools = require("dko.tools")
 tools.register({
   name = "dartls",
   runner = "lspconfig",
-  lspconfig = function()
-    ---@type lspconfig.Config
-    return {
-      settings = {
-        dart = { showTodos = false },
-      },
-    }
-  end,
 })
