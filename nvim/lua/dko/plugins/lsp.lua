30bc6627cd521b5e2df37082c4e0170f5993a446
David O'Trakoun <me@davidosomething.com>
2025-09-29 09:06:53 -0400

    add lensline

--- nvim/lua/dko/plugins/lsp.lua
+++ nvim/lua/dko/plugins/lsp.lua
@@ -124,4 +124,24 @@ return {
       resolve_config_and_enable(dkotools.mason_lspconfig_resolvers)
     end,
   },
+
+  {
+    "oribarilan/lensline.nvim",
+    tag = "2.0.0", -- or: branch = 'release/2.x' for latest non-breaking updates
+    event = "LspAttach",
+    config = function()
+      require("lensline").setup({
+        profiles = {
+          {
+            name = "minimal",
+            style = {
+              placement = "inline",
+              prefix = "",
+              -- render = "focused", optionally render lenses only for focused function
+            },
+          },
+        },
+      })
+    end,
+  },
 }
