487d11f8eae46b501956f14c8b63f3dffc5db1f1
David O'Trakoun <dotrakoun@clearstreet.io>
2025-10-16 10:26:43 -0400

    no ai plugins, they all suck

--- nvim/after/ftplugin/gitcommit.lua
+++ nvim/after/ftplugin/gitcommit.lua
@@ -2,5 +2,8 @@
 vim.opt_local.complete:append({ "kspell" })
 
 vim.bo.comments = ":;"
+--- ensure `gcc` formats properly
+vim.bo.commentstring = "; %s"
+
 vim.bo.swapfile = false
 vim.bo.textwidth = 80
