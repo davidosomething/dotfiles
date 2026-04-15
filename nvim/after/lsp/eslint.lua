07a88479110c8c789ac0edc190b623bfb8206809
David O'Trakoun <dotrakoun@clearstreet.io>
2026-04-15 08:29:52 -0400

    disable pnpm-catalog-info for now, do i want it?

---
+++ nvim/after/lsp/eslint.lua
@@ -0,0 +1,14 @@
+---@type vim.lsp.Config
+return {
+  filetypes = {
+    "javascript",
+    "javascriptreact",
+    "typescript",
+    "typescriptreact",
+    "vue",
+    "svelte",
+    "astro",
+    "htmlangular",
+    "yaml",
+  },
+}
