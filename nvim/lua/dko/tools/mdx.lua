5c36dbea0be53d3d7d69ce6116bcafe858a9f3b6
David O'Trakoun <dotrakoun@clearstreet.io>
2026-05-02 23:40:56 -0400

    mdx not working

--- nvim/lua/dko/tools/mdx.lua
+++ nvim/lua/dko/tools/mdx.lua
@@ -1,6 +1,8 @@
 local tools = require("dko.tools")
 
 -- can't get it to find typescript in my nx project
+-- also this issue https://github.com/neovim/neovim/issues/37204
+-- LSP[mdx_analyzer]: Error SERVER_REQUEST_HANDLER_ERROR: "...2.2/nvim-macos-arm64/share/nvim/runtime/lua/vim/glob.lua:373: Invalid glob: **/*.{mdx}"
 -- tools.register({
 --   name = "mdx_analyzer",
 --   runner = "lspconfig",
