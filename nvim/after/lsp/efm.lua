e297d95077ff700f1c29dfda48b462471c793d86
David O'Trakoun <me@davidosomething.com>
2026-05-25 18:36:43 -0400

    fix not linting yaml on open

--- nvim/after/lsp/efm.lua
+++ nvim/after/lsp/efm.lua
@@ -1,10 +1,15 @@
 --- https://github.com/neovim/nvim-lspconfig/blob/master/lsp/efm.lua
 
+local defaults = {
+  lintAfterOpen = true,
+  lintIgnoreExitCode = true,
+}
+
 local configs_by_ft = require("dko.tools").config_with_efm_by_ft
 local filetypes = vim.tbl_keys(configs_by_ft)
 local languages = vim.tbl_map(function(configs)
   return vim.tbl_map(function(c)
-    return c.efm()
+    return vim.tbl_extend("keep", c.efm(), defaults)
   end, configs)
 end, configs_by_ft)
 
